8d76d93be0a0648f17327cb365eece9a14689715
Giulia Campana <giuliaecampana@gmail.com>
2020-01-21 12:23:53 -0800

    ADd property FQNs

--- src/core/edm/constants/FullyQualifiedNames.js
+++ src/core/edm/constants/FullyQualifiedNames.js
@@ -24,7 +24,30 @@ const APP_TYPE_FQNS :Object = {
 };
 
 const PROPERTY_TYPE_FQNS = {
+  CITY: new FullyQualifiedName('location.city'),
+  COUNTRY: new FullyQualifiedName('ol.country'),
+  DATE: new FullyQualifiedName('general.date'),
+  DOB: new FullyQualifiedName('nc.PersonBirthDate'),
+  EMAIL: new FullyQualifiedName('staff.email'),
+  ETHNICITY: new FullyQualifiedName('nc.PersonEthnicity'),
+  FIRST_NAME: new FullyQualifiedName('nc.PersonGivenName'),
+  GENDER: new FullyQualifiedName('bhr.gender'),
+  HIGHEST_EDUCATION_LEVEL: new FullyQualifiedName('person.highesteducation'),
+  LAST_NAME: new FullyQualifiedName('nc.PersonSurName'),
+  MARITAL_STATUS: new FullyQualifiedName('person.maritalstatus'),
+  MIDDLE_NAME: new FullyQualifiedName('nc.PersonMiddleName'),
   OL_ID_FQN: new FullyQualifiedName('ol.id'),
+  PHONE_NUMBER: new FullyQualifiedName('contact.phonenumber'),
+  PREFERRED: new FullyQualifiedName('ol.preferred'),
+  PREFERRED_METHOD_OF_CONTACT: new FullyQualifiedName('ol.preferredmethod'),
+  PROJECTED_RELEASE_DATETIME: new FullyQualifiedName('ol.projectedreleasedatetime'),
+  RACE: new FullyQualifiedName('nc.PersonRace'),
+  SEX_OFFENDER: new FullyQualifiedName('ol.sexoffender'),
+  SSN: new FullyQualifiedName('nc.SSN'),
+  STREET: new FullyQualifiedName('location.street'),
+  TYPE: new FullyQualifiedName('ol.type'),
+  US_STATE: new FullyQualifiedName('location.state'),
+  ZIP: new FullyQualifiedName('location.zip'),
 };
 
 export {
