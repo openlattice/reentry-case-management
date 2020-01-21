125d26226e155c1dca1325427519f22210a2ca84
Giulia Campana <giuliaecampana@gmail.com>
2020-01-20 18:11:03 -0800

    Redux state constants

---
+++ src/utils/constants/ReduxStateConstants.js
@@ -0,0 +1,19 @@
+// @flow
+
+const SHARED :Object = {
+  ACTIONS: 'actions',
+  REQUEST_STATE: 'requestState',
+};
+
+const APP :Object = {
+  APP: 'app',
+  ENTITY_SET_IDS_BY_ORG_ID: 'entitySetIdsByOrgId',
+  INITIALIZE_APPLICATION: 'initializeApplication',
+  ORGS: 'organizations',
+  SELECTED_ORG_ID: 'selectedOrgId',
+};
+
+export {
+  APP,
+  SHARED,
+};
