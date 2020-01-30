31ada5477f2607a8e80c1caab37c987695f08566
Giulia Campana <giuliaecampana@gmail.com>
2020-01-29 18:43:48 -0800

    Start on Releases Search

--- src/containers/releases/Releases.js
+++ src/containers/releases/Releases.js
@@ -1,8 +1,89 @@
 // @flow
 import React from 'react';
+import styled from 'styled-components';
+import {
+  Button,
+  Card,
+  CardSegment,
+  DatePicker,
+  Input,
+} from 'lattice-ui-kit';
+
+import COLORS from '../../core/style/Colors';
+
+const FIELDS :Object = [
+  {
+    label: 'Last name',
+    field: <Input />
+  },
+  {
+    label: 'First name',
+    field: <Input />
+  },
+  {
+    label: 'Start date',
+    field: <DatePicker />
+  },
+  {
+    label: 'End date',
+    field: <DatePicker />
+  },
+];
+
+const ContainerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const HeaderRowWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 22px;
+  width: 100%;
+`;
+
+const Header = styled.div`
+  color: ${COLORS.GRAY_01};
+  font-size: 26px;
+  font-weight: 600;
+  line-height: 35px;
+`;
+
+const StyledButton = styled(Button)`
+  font-size: 14px;
+  padding: 8px 32px;
+`;
+
+const Grid = styled.div`
+  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
+`;
 
 const Releases = () => (
-  <div>Releases</div>
+  <ContainerWrapper>
+    <HeaderRowWrapper>
+      <Header>New Releases</Header>
+      <StyledButton mode="primary">New Intake</StyledButton>
+    </HeaderRowWrapper>
+    <Card>
+      <CardSegment padding="30px">
+        <Grid>
+          {
+            FIELDS.map((fieldObject :Object) => (
+              <div key={fieldObject.label}>
+                <div>{fieldObject.label}</div>
+                {fieldObject.field}
+              </div>
+            ))
+          }
+          <Button mode="primary">Search</Button>
+        </Grid>
+      </CardSegment>
+    </Card>
+  </ContainerWrapper>
 );
 
 export default Releases;
---
+++ src/core/style/Colors.js
@@ -0,0 +1,7 @@
+
+// @flow
+
+const COLORS :Object = {
+  GRAY_01: '#2E2E34'
+};
+export default COLORS;
