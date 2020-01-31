e937edea4d5f2de6c4d9ae4c5ca7d60ef0d19f9e
Giulia Campana <giuliaecampana@gmail.com>
2020-01-30 19:01:00 -0800

    add a DateTimeUtil

--- src/utils/DateTimeUtils.js
+++ src/utils/DateTimeUtils.js
@@ -1,19 +1,24 @@
-/*
- * @flow
- */
-
+//  @flow
 import { DateTime } from 'luxon';
 
-function formatAsDate(value :string = '') :string {
-
+const formatAsDate = (value :string = '') :string => {
   const date = DateTime.fromISO(value);
   if (date.isValid) {
     return date.toLocaleString(DateTime.DATE_SHORT);
   }
-
   return '';
-}
+};
+
+const checkIfDatesAreEqual = (isoDate1 :string, isoDate2 :string) => {
+  const dateObj1 :DateTime = DateTime.fromISO(isoDate1);
+  const dateObj2 :DateTime = DateTime.fromISO(isoDate2);
+  if (!dateObj1.isValid || !dateObj2.isValid) {
+    return false;
+  }
+  return dateObj1.hasSame(dateObj2, 'millisecond');
+};
 
 export {
+  checkIfDatesAreEqual,
   formatAsDate,
 };
