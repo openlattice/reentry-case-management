1a76c6c0e50a57cf545c1aa076fd601c23766a0d
Giulia Campana <giuliaecampana@gmail.com>
2020-02-12 15:52:13 -0800

    finish util for aggregating search results data

--- src/containers/participants/utils/ParticipantsUtils.js
+++ src/containers/participants/utils/ParticipantsUtils.js
@@ -2,12 +2,20 @@
 import { List, Map, fromJS } from 'immutable';
 import { DateTime } from 'luxon';
 
-import { getEntityProperties } from '../../../utils/DataUtils';
-import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
+import { sortEntitiesByDateProperty } from '../../../utils/Utils';
+import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
-const { DOB, FIRST_NAME, LAST_NAME } = PROPERTY_TYPE_FQNS;
+const { MANUAL_JAIL_STAYS, NEEDS_ASSESSMENT } = APP_TYPE_FQNS;
+const {
+  DATETIME_COMPLETED,
+  DOB,
+  FIRST_NAME,
+  LAST_NAME,
+  PROJECTED_RELEASE_DATETIME
+} = PROPERTY_TYPE_FQNS;
 
-const aggregateResultsData = (people :List) :List => {
+const aggregateResultsData = (people :List, personNeighbors :Map, jailNamesByJailStayEKID :Map) :List => {
 
   let data :List = List();
   people.forEach((person :Map) => {
@@ -19,12 +27,22 @@ const aggregateResultsData = (people :List) :List => {
     ]);
     const dateOfBirth :string = DateTime.fromISO(dob).toLocaleString(DateTime.DATE_SHORT);
 
+    const personEKID :UUID = getEKID(person);
+    const jailStays :List = personNeighbors.getIn([personEKID, MANUAL_JAIL_STAYS]).valueSeq().toList();
+    const jailStay :Map = sortEntitiesByDateProperty(jailStays, [PROJECTED_RELEASE_DATETIME]).last();
+    const jailStayEKID :UUID = getEKID(jailStay);
+    const jailName :string = jailNamesByJailStayEKID.get(jailStayEKID);
+
+    const needsAssessment :Map = personNeighbors.getIn([personEKID, NEEDS_ASSESSMENT]).valueSeq().last();
+    // $FlowFixMe
+    const { [DATETIME_COMPLETED]: enrollmentDateTime } = getEntityProperties(needsAssessment, [DATETIME_COMPLETED]);
+    const enrollmentDate :string = DateTime.fromISO(enrollmentDateTime).toLocaleString(DateTime.DATE_SHORT);
+
     const personDataObject :Map = fromJS({
       name: `${firstName} ${lastName}`,
       dob: dateOfBirth,
-      idNumber: '',
-      jail: '',
-      enrollmentDate: '',
+      jail: jailName,
+      enrollmentDate,
     });
     data = data.push(personDataObject);
   });
