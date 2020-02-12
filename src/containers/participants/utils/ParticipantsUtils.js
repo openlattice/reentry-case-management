f7b322af9f6bc852b4f180fbea6a98c31f26f3b0
Giulia Campana <giuliaecampana@gmail.com>
2020-02-12 12:11:22 -0800

    change util to aggregate data for SearchResults

--- src/containers/participants/utils/ParticipantsUtils.js
+++ src/containers/participants/utils/ParticipantsUtils.js
@@ -1,16 +1,15 @@
 // @flow
-import { List, Map } from 'immutable';
+import { List, Map, fromJS } from 'immutable';
 import { DateTime } from 'luxon';
 
-import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
+import { getEntityProperties } from '../../../utils/DataUtils';
 import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { TABLE_HEADERS } from '../constants';
 
 const { DOB, FIRST_NAME, LAST_NAME } = PROPERTY_TYPE_FQNS;
 
-const aggregateTableData = (people :List) :Object[] => {
+const aggregateResultsData = (people :List) :List => {
 
-  const data :Object[] = [];
+  let data :List = List();
   people.forEach((person :Map) => {
     // $FlowFixMe
     const { [DOB]: dob, [FIRST_NAME]: firstName, [LAST_NAME]: lastName } = getEntityProperties(person, [
@@ -19,19 +18,20 @@ const aggregateTableData = (people :List) :Object[] => {
       LAST_NAME
     ]);
     const dateOfBirth :string = DateTime.fromISO(dob).toLocaleString(DateTime.DATE_SHORT);
-    const personEKID :UUID = getEKID(person);
 
-    const personDataObject :Object = {
-      [TABLE_HEADERS[0]]: `${firstName} ${lastName}`,
-      [TABLE_HEADERS[1]]: dateOfBirth,
-      id: personEKID,
-    };
-    data.push(personDataObject);
+    const personDataObject :Map = fromJS({
+      name: `${firstName} ${lastName}`,
+      dob: dateOfBirth,
+      idNumber: '',
+      jail: '',
+      enrollmentDate: '',
+    });
+    data = data.push(personDataObject);
   });
   return data;
 };
 
 /* eslint-disable import/prefer-default-export */
 export {
-  aggregateTableData,
+  aggregateResultsData,
 };
