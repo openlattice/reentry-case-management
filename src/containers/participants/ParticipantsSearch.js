b2855f91820ae24deaccb88ac3563d220db4030a
Giulia Campana <giuliaecampana@gmail.com>
2020-02-11 18:52:19 -0800

    create a NoResults componentn for participants search

--- src/containers/participants/ParticipantsSearch.js
+++ src/containers/participants/ParticipantsSearch.js
@@ -6,6 +6,7 @@ import {
   Badge,
   Card,
   CardSegment,
+  CardStack,
   DatePicker,
   Input,
   Label,
@@ -15,16 +16,25 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import type { RequestSequence, RequestState } from 'redux-reqseq';
 
+import NoResults from '../../components/noresults/NoResults';
+
 import { ButtonWrapper, FieldsGrid, StyledSearchButton } from '../../components/search/SearchStyledComponents';
 import { isNonEmptyString } from '../../utils/LangUtils';
 import { requestIsPending, requestIsSuccess } from '../../utils/RequestStateUtils';
+import { generateTableHeaders } from '../../utils/Utils';
+import { aggregateTableData } from './utils/ParticipantsUtils';
+import { TABLE_HEADERS } from './constants';
 import { SEARCH_PARTICIPANTS, searchParticipants } from './ParticipantsActions';
-import { PARTICIPANTS, SHARED } from '../../utils/constants/ReduxStateConstants';
+import { APP, PARTICIPANTS, SHARED } from '../../utils/constants/ReduxStateConstants';
 import { COLORS } from '../../core/style/Colors';
+import { APP_TYPE_FQNS } from '../../core/edm/constants/FullyQualifiedNames';
 
+const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
 const { ACTIONS, REQUEST_STATE, TOTAL_HITS } = SHARED;
 const { SEARCHED_PARTICIPANTS } = PARTICIPANTS;
-const MAX_HITS :number = 20;
+const { PEOPLE } = APP_TYPE_FQNS;
+const MAX_HITS :number = 5;
+const participantsHeaders :Object[] = generateTableHeaders(TABLE_HEADERS);
 
 const SearchGrid = styled(FieldsGrid)`
   margin-top: 20px;
@@ -44,10 +54,15 @@ const TableHeader = styled(CardSegment)`
   font-weight: 600;
 `;
 
+const TableName = styled.div`
+  margin-right: 10px;
+`;
+
 type Props = {
   actions :{
     searchParticipants :RequestSequence;
   };
+  entitySetIdsByFqn :Map;
   requestStates :{
     SEARCH_PARTICIPANTS :RequestState;
   };
@@ -71,8 +86,21 @@ class ParticipantsSearch extends Component<Props, State> {
       dob: '',
       firstName: '',
       lastName: '',
-      page: 0,
+      // page: 0,
     };
+  }
+
+  componentDidMount() {
+    const { actions, entitySetIdsByFqn } = this.props;
+    if (entitySetIdsByFqn.has(PEOPLE)) {
+      actions.searchParticipants({
+        dob: '',
+        firstName: '',
+        lastName: '',
+        maxHits: MAX_HITS,
+        start: 0,
+      });
+    }
   }
 
   searchPeople = (e :SyntheticEvent<HTMLInputElement> | void, startIndex :?number) => {
@@ -99,21 +127,32 @@ class ParticipantsSearch extends Component<Props, State> {
     this.setState({ dob: date });
   }
 
-  setPage = (page :number) => {
-    this.setState({ page });
-  }
+  // setPage = (page :number) => {
+  //   this.setState({ page });
+  // }
 
-  onPageChange= ({ page: newPage, start } :Object) => {
-    this.searchPeople(undefined, start);
-    this.setPage(newPage);
+  onPageChange= (payload :Object) => {
+    console.log('payload: ', payload);
+    debugger;
+    const { actions } = this.props;
+    actions.searchParticipants({
+      dob: '',
+      firstName: '',
+      lastName: '',
+      maxHits: MAX_HITS,
+      start: payload.start,
+    });
+    // this.setPage(newPage);
   }
 
   render() {
-    const { requestStates, totalHits } = this.props;
+    const { searchedParticipants, requestStates, totalHits } = this.props;
     const isSearching :boolean = requestIsPending(requestStates[SEARCH_PARTICIPANTS]);
     const searchWasSuccessful :boolean = requestIsSuccess(requestStates[SEARCH_PARTICIPANTS]);
+    const tableData :Object[] = aggregateTableData(searchedParticipants);
+    console.log(tableData)
     return (
-      <>
+      <CardStack>
         <Card>
           <CardSegment padding="30px" vertical>
             <div>Search Participants</div>
@@ -135,36 +174,48 @@ class ParticipantsSearch extends Component<Props, State> {
                 <DatePicker onChange={this.onDateChange} />
               </div>
               <ButtonWrapper>
-                <StyledSearchButton onClick={() => {}}>Search</StyledSearchButton>
+                <StyledSearchButton onClick={this.searchPeople}>Search</StyledSearchButton>
               </ButtonWrapper>
             </SearchGrid>
           </CardSegment>
         </Card>
-        {
-          (isSearching || searchWasSuccessful) && (
-            <TableCard>
-              <TableHeader padding="50px 50px 40px 50px">
-                <div>All Participants</div>
-                <Badge mode="primary" count={totalHits} />
-              </TableHeader>
-              <Table
-                  isLoading={isSearching}
-                  onPageChange={this.onPageChange}
-                  paginated
-                  totalRows={MAX_HITS} />
-            </TableCard>
-          )
-        }
-      </>
+        <TableCard>
+          {
+            tableData.length ? (
+              <>
+                <TableHeader padding="50px 50px 40px 50px">
+                  <TableName>All Participants</TableName>
+                  <Badge mode="primary" count={totalHits} />
+                </TableHeader>
+                <Table
+                    data={tableData}
+                    exact
+                    headers={participantsHeaders}
+                    isLoading={isSearching}
+                    onPageChange={this.onPageChange}
+                    paginated
+                    rowsPerPageOptions={[MAX_HITS, 10]}
+                    totalRows={totalHits} />
+              </>
+            )
+              : (
+                <NoResults text="No Participants Found" />
+              )
+          }
+        </TableCard>
+      </CardStack>
     );
   }
 }
 
 const mapStateToProps = (state :Map) => {
+  const app = state.get(APP.APP);
   const participants = state.get(PARTICIPANTS.PARTICIPANTS);
+  const selectedOrgId :UUID = app.get(SELECTED_ORG_ID, '');
   return {
     [SEARCHED_PARTICIPANTS]: participants.get(SEARCHED_PARTICIPANTS),
     [TOTAL_HITS]: participants.get(TOTAL_HITS),
+    entitySetIdsByFqn: app.getIn([ENTITY_SET_IDS_BY_ORG_ID, selectedOrgId]),
     requestStates: {
       [SEARCH_PARTICIPANTS]: participants.getIn([ACTIONS, SEARCH_PARTICIPANTS, REQUEST_STATE]),
     }
