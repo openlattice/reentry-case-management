3e0e05f7a6baa884933907ac1d6d0e78525aedf6
Giulia Campana <giuliaecampana@gmail.com>
2020-02-13 19:04:45 -0800

    finish util for profile card based on new neighbors map

--- src/containers/profile/utils/ProfileUtils.js
+++ src/containers/profile/utils/ProfileUtils.js
@@ -1,46 +1,63 @@
 // @flow
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 import { DateTime } from 'luxon';
 
 import { getEntityProperties } from '../../../utils/DataUtils';
 import { getPersonAge } from '../../../utils/PeopleUtils';
-import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
+const { CONTACT_INFO, PERSON_DETAILS } = APP_TYPE_FQNS;
 const {
   DOB,
+  EMAIL,
+  ETHNICITY,
   FIRST_NAME,
+  GENDER,
   LAST_NAME,
   MIDDLE_NAME,
+  PHONE_NUMBER,
+  PREFERRED,
   RACE,
 } = PROPERTY_TYPE_FQNS;
 
-const getFormattedParticipantData = (participant :Map) :Map => {
+const getFormattedParticipantData = (participant :Map, participantNeighbors :Map) :Map => {
 
   const age = getPersonAge(participant);
   const {
     // $FlowFixMe
     [DOB]: dobISO,
     // $FlowFixMe
+    [ETHNICITY]: ethnicity,
+    // $FlowFixMe
     [FIRST_NAME]: firstName,
     // $FlowFixMe
     [LAST_NAME]: lastName,
     // $FlowFixMe
-    [MIDDLE_NAME]: middleName,
-    // $FlowFixMe
     [RACE]: race
   } = getEntityProperties(participant, [DOB, FIRST_NAME, LAST_NAME, MIDDLE_NAME, RACE]);
-
   const dob :string = DateTime.fromISO(dobISO).toLocaleString(DateTime.DATE_SHORT);
+  const personDetails :List = participantNeighbors.get(PERSON_DETAILS, List());
+  let gender :string = '';
+  if (!personDetails.isEmpty()) gender = personDetails.getIn([0, GENDER]);
+  const contactInfoEntities :List = participantNeighbors.get(CONTACT_INFO, List());
+  let preferredContact :string = '';
+  const preferredContactEntity :Map = contactInfoEntities.find((contact :Map) => contact.has(PREFERRED));
+  if (preferredContactEntity && preferredContactEntity.has(EMAIL)) {
+    preferredContact = preferredContactEntity.getIn([EMAIL, 0]);
+  }
+  if (preferredContactEntity && preferredContactEntity.has(PHONE_NUMBER)) {
+    preferredContact = preferredContactEntity.getIn([PHONE_NUMBER, 0]);
+  }
 
   const participantData = Map({
     lastName,
-    middleName,
     firstName,
-    phoneNumber: '---',
     dob,
     age,
-    gender: '---',
+    gender,
     race,
+    ethnicity,
+    preferredContact,
   });
   return participantData;
 };
