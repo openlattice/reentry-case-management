d4731104b988433e3aea43ff6091cf643c4ac5c2
Giulia Campana <giuliaecampana@gmail.com>
2020-02-20 18:17:07 -0800

    make modal header component that will be used across reentry

---
+++ src/components/modal/ModalHeader.js
@@ -0,0 +1,44 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { CardSegment, Colors } from 'lattice-ui-kit';
+import { faTimes } from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const { NEUTRALS } = Colors;
+
+const ModalCardSegment = styled(CardSegment)`
+  justify-content: space-between;
+`;
+
+const ModalTitle = styled.div`
+  color: ${NEUTRALS[0]};
+  font-weight: 600;
+  font-size: 22px;
+  max-height: 30px;
+`;
+
+const CloseButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 12px;
+  height: 32px;
+  width: 32px;
+`;
+
+type HeaderProps = {
+  onClose :() => void;
+  title :string;
+};
+
+const ModalHeader = ({ onClose, title } :HeaderProps) => (
+  <ModalCardSegment padding="30px 30px 20px">
+    <ModalTitle>{ title }</ModalTitle>
+    <CloseButton onClick={onClose}>
+      <FontAwesomeIcon color={NEUTRALS[2]} icon={faTimes} size="lg" />
+    </CloseButton>
+  </ModalCardSegment>
+);
+
+export default ModalHeader;
