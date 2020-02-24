f400cd29ed9d9fa5a0e46019e41c4c58f0a26851
Giulia Campana <giuliaecampana@gmail.com>
2020-02-24 12:45:26 -0800

    util for prepopulating edit form data

--- src/containers/providers/utils/ProvidersUtils.js
+++ src/containers/providers/utils/ProvidersUtils.js
@@ -1,12 +1,32 @@
 // @flow
 import { List, Map, get } from 'immutable';
+import { DataProcessingUtils } from 'lattice-fabricate';
 
 import { EMPTY_FIELD, getPersonFullName } from '../../../utils/FormattingUtils';
-import { getEKID } from '../../../utils/DataUtils';
+import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
 import { isDefined } from '../../../utils/LangUtils';
-import { PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
-const { EMAIL, PHONE_NUMBER } = PROPERTY_TYPE_FQNS;
+const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
+const {
+  CONTACT_INFO,
+  LOCATION,
+  PROVIDER,
+  PROVIDER_STAFF,
+} = APP_TYPE_FQNS;
+const {
+  CITY,
+  DESCRIPTION,
+  EMAIL,
+  FIRST_NAME,
+  LAST_NAME,
+  NAME,
+  PHONE_NUMBER,
+  STREET,
+  TYPE,
+  US_STATE,
+  ZIP,
+} = PROPERTY_TYPE_FQNS;
 
 const getListOfContacts = (staffEntities :List, contactInfoByContactPersonEKID :Map) :List => {
   let data :List = List();
@@ -31,6 +51,57 @@ const getListOfContacts = (staffEntities :List, contactInfoByContactPersonEKID :
   return data;
 };
 
+const getDataForFormPrepopulation = (
+  provider :Map,
+  address :Map,
+  providerStaff :List,
+  contactInfoByContactPersonEKID :Map
+) :Object => {
+
+  const { [DESCRIPTION]: providerDescription, [NAME]: providerName, [TYPE]: types } = getEntityProperties(
+    provider,
+    [DESCRIPTION, NAME, TYPE]
+  );
+  const providerTypes :string[] = typeof types === 'string' ? [types] : types;
+  const {
+    [CITY]: city,
+    [STREET]: streetAddress,
+    [US_STATE]: state,
+    [ZIP]: zipCode,
+  } = getEntityProperties(address, [CITY, STREET, US_STATE, ZIP]);
+
+  const pointsOfContact :Object[] = [];
+  providerStaff.forEach((staff :Map) => {
+    const pointOfContact :Object = {};
+    const { [FIRST_NAME]: firstName, [LAST_NAME]: lastName } = getEntityProperties(staff, [FIRST_NAME, LAST_NAME]);
+    pointOfContact[getEntityAddressKey(-1, PROVIDER_STAFF, FIRST_NAME)] = firstName;
+    pointOfContact[getEntityAddressKey(-1, PROVIDER_STAFF, LAST_NAME)] = lastName;
+    const staffEKID :UUID = getEKID(staff);
+    const contacts :List = contactInfoByContactPersonEKID.get(staffEKID, List());
+    const phoneEntity :any = contacts.find((contact :Map) => isDefined(get(contact, PHONE_NUMBER)));
+    if (isDefined(phoneEntity)) {
+      pointOfContact[getEntityAddressKey(-1, CONTACT_INFO, PHONE_NUMBER)] = get(phoneEntity, PHONE_NUMBER);
+    }
+    const emailEntity :any = contacts.find((contact :Map) => isDefined(get(contact, EMAIL)));
+    if (isDefined(emailEntity)) {
+      pointOfContact[getEntityAddressKey(-2, CONTACT_INFO, EMAIL)] = get(emailEntity, EMAIL);
+    }
+    pointsOfContact.push(pointOfContact);
+  });
+
+  return {
+    city,
+    pointsOfContact,
+    providerDescription,
+    providerName,
+    providerTypes,
+    state,
+    streetAddress,
+    zipCode,
+  };
+};
+
 export {
+  getDataForFormPrepopulation,
   getListOfContacts,
 };
