d1a310d40cdaa04cf21a6790bb9aa9d8bbe42ba9
Giulia Campana <giuliaecampana@gmail.com>
2020-03-09 14:18:54 -0700

    add table to reports page

--- src/containers/reports/Reports.js
+++ src/containers/reports/Reports.js
@@ -1,14 +1,29 @@
 // @flow
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Button, Table } from 'lattice-ui-kit';
+import { Map } from 'immutable';
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardSegment,
+  Table
+} from 'lattice-ui-kit';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import type { RequestSequence } from 'redux-reqseq';
+import type { RequestSequence, RequestState } from 'redux-reqseq';
 
 import DownloadPeopleModal from './DownloadPeopleModal';
 import COLORS from '../../core/style/Colors';
-import { getReportsData } from './ReportsActions';
+import { generateTableHeaders } from '../../utils/Utils';
+import { requestIsPending } from '../../utils/RequestStateUtils';
+import { GET_REPORTS_DATA, getReportsData } from './ReportsActions';
+import { TABLE_HEADERS } from './ReportsConstants';
+import { APP, REPORTS, SHARED } from '../../utils/constants/ReduxStateConstants';
+
+const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
+const { ACTIONS, REQUEST_STATE } = SHARED;
+const { SERVICES_TABLE_DATA } = REPORTS;
 
 const HeaderRow = styled.div`
   align-items: center;
@@ -24,28 +39,74 @@ const Header = styled.div`
   line-height: 35px;
 `;
 
+const TableCard = styled(Card)`
+  & > ${CardSegment} {
+    border: none;
+  }
+`;
+
+const TableHeader = styled(CardHeader)`
+  border: none;
+  color: ${COLORS.GRAY_01};
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 type Props = {
   actions :{
     getReportsData :RequestSequence;
   };
+  entitySetIdsByFqn :Map;
+  requestStates :{
+    GET_REPORTS_DATA :RequestState;
+  };
+  servicesTableData :Object[];
 };
 
-const Reports = ({ actions } :Props) => {
+const Reports = ({
+  actions,
+  entitySetIdsByFqn,
+  requestStates,
+  servicesTableData
+} :Props) => {
   const [downloadModalVisible, setModalVisibility] = useState(false);
   useEffect(() => {
-    actions.getReportsData();
-  }, [actions]);
+    if (!entitySetIdsByFqn.isEmpty()) actions.getReportsData();
+  }, [actions, entitySetIdsByFqn]);
+  const tableHeaders :Object[] = generateTableHeaders(TABLE_HEADERS);
+  const tableIsLoading :boolean = requestIsPending(requestStates[GET_REPORTS_DATA]);
   return (
     <>
       <HeaderRow>
         <Header>Reports</Header>
         <Button mode="primary" onClick={() => setModalVisibility(true)}>Download</Button>
       </HeaderRow>
+      <TableCard>
+        <TableHeader>Most Utilized Services</TableHeader>
+        <CardSegment padding="0">
+          <Table
+              data={servicesTableData}
+              headers={tableHeaders}
+              isLoading={tableIsLoading} />
+        </CardSegment>
+      </TableCard>
       <DownloadPeopleModal
           isVisible={downloadModalVisible}
           onClose={() => setModalVisibility(false)} />
     </>
   );
+};
+
+const mapStateToProps = (state :Map) => {
+  const reports :Map = state.get(REPORTS.REPORTS);
+  const selectedOrgId :UUID = state.getIn([APP.APP, SELECTED_ORG_ID]);
+  return {
+    [SERVICES_TABLE_DATA]: reports.get(SERVICES_TABLE_DATA),
+    entitySetIdsByFqn: state.getIn([APP.APP, ENTITY_SET_IDS_BY_ORG_ID, selectedOrgId], Map()),
+    requestStates: {
+      [GET_REPORTS_DATA]: reports.getIn([ACTIONS, GET_REPORTS_DATA, REQUEST_STATE]),
+    }
+  };
 };
 
 const mapDispatchToProps = (dispatch :Function) => ({
@@ -55,4 +116,4 @@ const mapDispatchToProps = (dispatch :Function) => ({
 });
 
 // $FlowFixMe
-export default connect(null, mapDispatchToProps)(Reports);
+export default connect(mapStateToProps, mapDispatchToProps)(Reports);
