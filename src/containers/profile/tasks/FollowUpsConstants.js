dc3f08c41be5234553d29f5e033f1c619726ef52
Giulia Campana <giuliaecampana@gmail.com>
2020-03-11 14:20:23 -0700

    Follow up consts

---
+++ src/containers/profile/tasks/FollowUpsConstants.js
@@ -0,0 +1,17 @@
+// @flow
+
+const FOLLOW_UPS_CATEGORIES :Object = {
+  TASK: 'Task',
+  MEETING: 'Meeting'
+};
+
+const FOLLOW_UPS_TASK_TYPES :string[] = [
+  'Schedule Phone Call',
+  'Schedule Interview',
+  'Submit Application',
+];
+
+export {
+  FOLLOW_UPS_CATEGORIES,
+  FOLLOW_UPS_TASK_TYPES,
+};
