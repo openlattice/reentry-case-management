4acfdfce0ad520fff5e4fd775631c82dbbba2d3d
Giulia Campana <giuliaecampana@gmail.com>
2020-03-10 18:16:20 -0700

    add headers to tasks manager

--- src/containers/profile/tasks/ParticipantTasks.js
+++ src/containers/profile/tasks/ParticipantTasks.js
@@ -1,8 +1,134 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Map } from 'immutable';
+import {
+  Breadcrumbs,
+  Button,
+  Colors,
+  Spinner
+} from 'lattice-ui-kit';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { RequestSequence, RequestState } from 'redux-reqseq';
+import type { Match } from 'react-router';
 
-const ParticipantTasks = () => (
-  <div>Tasks</div>
-);
+import * as Routes from '../../../core/router/Routes';
+import { Header, NameHeader } from '../styled/GeneralProfileStyles';
+import { goToRoute } from '../../../core/router/RoutingActions';
+import { getPersonFullName } from '../../../utils/PeopleUtils';
+import { requestIsPending } from '../../../utils/RequestStateUtils';
+import { LOAD_TASKS, loadTasks } from './TasksActions';
+import { PARTICIPANT_TASKS, PROFILE, SHARED } from '../../../utils/constants/ReduxStateConstants';
+import type { GoToRoute } from '../../../core/router/RoutingActions';
 
-export default ParticipantTasks;
+const { NEUTRALS } = Colors;
+const { PARTICIPANT, PARTICIPANT_NEIGHBORS } = PROFILE;
+const { ACTIONS, REQUEST_STATE } = SHARED;
+
+const HeaderRow = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 36px;
+`;
+
+const PageHeader = styled.div`
+  color: ${NEUTRALS[0]};
+  font-size: 28px;
+  font-weight: 600;
+`;
+
+type Props = {
+  actions :{
+    goToRoute :GoToRoute;
+    loadTasks :RequestSequence;
+  };
+  match :Match;
+  participant :Map;
+  participantNeighbors :Map;
+  requestStates :{
+    LOAD_TASKS :RequestState;
+  };
+};
+
+class ParticipantTasks extends Component<Props> {
+
+  componentDidMount() {
+    const {
+      actions,
+      match: {
+        params: { participantId }
+      }
+    } = this.props;
+    if (participantId) actions.loadTasks({ participantEKID: participantId });
+  }
+
+  goToProfile = () => {
+    const {
+      actions,
+      match: {
+        params: { participantId }
+      }
+    } = this.props;
+    if (participantId) actions.goToRoute(Routes.PARTICIPANT_PROFILE.replace(':participantId', participantId));
+  }
+
+  render() {
+    const {
+      match: {
+        params: { participantId }
+      },
+      participant,
+      requestStates,
+    } = this.props;
+
+    if (requestIsPending(requestStates[LOAD_TASKS])) {
+      return (
+        <Spinner size="2x" />
+      );
+    }
+
+    const participantName :string = getPersonFullName(participant);
+    const header :string = `${participantName}'s Tasks`;
+    return (
+      <>
+        <Breadcrumbs>
+          <Header to={Routes.PARTICIPANTS}>PARTICIPANTS</Header>
+          <NameHeader to={Routes.PARTICIPANT_PROFILE.replace(':participantId', participantId || '')}>
+            { participantName }
+          </NameHeader>
+          <NameHeader to={Routes.PARTICIPANT_TASK_MANAGER.replace(':participantId', participantId || '')}>
+            Manage Tasks
+          </NameHeader>
+        </Breadcrumbs>
+        <HeaderRow>
+          <PageHeader>{ header }</PageHeader>
+          <Button mode="primary">New Task</Button>
+        </HeaderRow>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state :Map) => {
+  const participantTasks = state.get(PARTICIPANT_TASKS.PARTICIPANT_TASKS);
+  const profile = state.get(PROFILE.PROFILE);
+  return {
+    [PARTICIPANT]: profile.get(PARTICIPANT),
+    [PARTICIPANT_NEIGHBORS]: profile.get(PARTICIPANT_NEIGHBORS),
+    requestStates: {
+      [LOAD_TASKS]: participantTasks.getIn([ACTIONS, LOAD_TASKS, REQUEST_STATE]),
+    }
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({
+    goToRoute,
+    loadTasks,
+  }, dispatch)
+});
+
+// $FlowFixMe
+export default connect(mapStateToProps, mapDispatchToProps)(ParticipantTasks);
