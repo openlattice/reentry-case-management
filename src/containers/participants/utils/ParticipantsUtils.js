2002c9afa3a4653f6db376d0508af27615e6d72d
Giulia Campana <giuliaecampana@gmail.com>
2020-03-11 18:05:09 -0700

    default list

--- src/containers/participants/utils/ParticipantsUtils.js
+++ src/containers/participants/utils/ParticipantsUtils.js
@@ -30,7 +30,7 @@ const aggregateResultsData = (people :List, personNeighbors :Map, jailNamesByJai
     const dateOfBirth :string = DateTime.fromISO(dob).toLocaleString(DateTime.DATE_SHORT);
 
     const personEKID :UUID = getEKID(person);
-    const jailStays :List = personNeighbors.getIn([personEKID, MANUAL_JAIL_STAYS]);
+    const jailStays :List = personNeighbors.getIn([personEKID, MANUAL_JAIL_STAYS], List());
     const jailStay :Map = sortEntitiesByDateProperty(jailStays, [PROJECTED_RELEASE_DATETIME]).last();
     const jailStayEKID :UUID = getEKID(jailStay);
     const jailName :string = jailNamesByJailStayEKID.get(jailStayEKID);
