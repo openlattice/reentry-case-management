13cc30d156380b7dcabdf453ef191988bd405af5
Giulia Campana <giuliaecampana@gmail.com>
2020-03-12 17:35:05 -0700

    add action for clearing success RS after submission

--- src/containers/profile/tasks/FollowUpsActions.js
+++ src/containers/profile/tasks/FollowUpsActions.js
@@ -2,6 +2,11 @@
 import { newRequestSequence } from 'redux-reqseq';
 import type { RequestSequence } from 'redux-reqseq';
 
+const CLEAR_ADD_REQUEST_STATE :'CLEAR_ADD_REQUEST_STATE' = 'CLEAR_ADD_REQUEST_STATE';
+const clearAddRequestState = () => ({
+  type: CLEAR_ADD_REQUEST_STATE
+});
+
 const CREATE_NEW_FOLLOW_UP :'CREATE_NEW_FOLLOW_UP' = 'CREATE_NEW_FOLLOW_UP';
 const createNewFollowUp :RequestSequence = newRequestSequence(CREATE_NEW_FOLLOW_UP);
 
@@ -16,10 +21,12 @@ const LOAD_TASKS :'LOAD_TASKS' = 'LOAD_TASKS';
 const loadTasks :RequestSequence = newRequestSequence(LOAD_TASKS);
 
 export {
+  CLEAR_ADD_REQUEST_STATE,
   CREATE_NEW_FOLLOW_UP,
   GET_ENTITIES_FOR_NEW_FOLLOW_UP_FORM,
   GET_FOLLOW_UP_NEIGHBORS,
   LOAD_TASKS,
+  clearAddRequestState,
   createNewFollowUp,
   getEntitiesForNewFollowUpForm,
   getFollowUpNeighbors,
