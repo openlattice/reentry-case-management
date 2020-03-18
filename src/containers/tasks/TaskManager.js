9474e98cb93fb708a79921d9387e99bf43671e75
Giulia Campana <giuliaecampana@gmail.com>
2020-03-17 18:32:33 -0700

    put tasks table also in task manager

--- src/containers/tasks/TaskManager.js
+++ src/containers/tasks/TaskManager.js
@@ -1,12 +1,41 @@
 // @flow
 import React from 'react';
+import styled from 'styled-components';
+import { Colors, Select } from 'lattice-ui-kit';
 
-const TaskManager = () => {
-  return (
-    <>
-      Task Manager Page.
-    </>
-  );
-};
+import TasksTable from '../../components/tasks/TasksTable';
+import { GrayerButton } from '../profile/styled/GeneralProfileStyles';
+
+const { NEUTRALS } = Colors;
+
+const PageHeader = styled.div`
+  color: ${NEUTRALS[0]};
+  font-size: 28px;
+  font-weight: 600;
+`;
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin: 25px 0;
+`;
+
+const SelectWrapper = styled.div`
+  width: 186px;
+`;
+
+const TaskManager = () => (
+  <>
+    <PageHeader>Task Manager</PageHeader>
+    <Row>
+      <SelectWrapper>
+        <Select />
+      </SelectWrapper>
+      <GrayerButton>New Task</GrayerButton>
+    </Row>
+    <TasksTable tasksData={[]} />
+  </>
+);
 
 export default TaskManager;
