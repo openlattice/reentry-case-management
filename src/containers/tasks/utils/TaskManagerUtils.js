77d701c19ab26d265c12f479e689291d24365af9
Giulia Campana <giuliaecampana@gmail.com>
2020-06-10 11:50:07 -0700

    add util for formatting tasks data for task manager

--- src/containers/tasks/utils/TaskManagerUtils.js
+++ src/containers/tasks/utils/TaskManagerUtils.js
@@ -1,13 +1,34 @@
 // @flow
+import { List, Map } from 'immutable';
+import { DateTime } from 'luxon';
 import { DataProcessingUtils } from 'lattice-fabricate';
 
+import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
+import { sortEntitiesByDateProperty } from '../../../utils/Utils';
+import { getPersonFullName } from '../../../utils/PeopleUtils';
+import { getTaskName } from '../../profile/tasks/utils/ParticipantFollowUpsUtils';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { FOLLOW_UPS_STATUSES } from '../../profile/tasks/FollowUpsConstants';
 
 const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
-const { PEOPLE } = APP_TYPE_FQNS;
-const { ENTITY_KEY_ID } = PROPERTY_TYPE_FQNS;
+const { PEOPLE, SUBJECT_OF } = APP_TYPE_FQNS;
+const {
+  CATEGORY,
+  ENTITY_KEY_ID,
+  DATETIME_COMPLETED,
+  DESCRIPTION,
+  GENERAL_DATETIME,
+  OL_TITLE,
+  STATUS,
+} = PROPERTY_TYPE_FQNS;
 
-const addLinkedPersonField = (schema :Object, uiSchema :Object) => {
+const getTaskNameForTaskManager = (category :string, title :string, personName :string) :string => {
+  let taskName = getTaskName(category, title, personName);
+  if (!taskName.includes(personName)) taskName = `${taskName} for ${personName}`;
+  return taskName;
+};
+
+const addLinkedPersonField = (schema :Object, uiSchema :Object) :Object => {
   const taskSchema = schema;
   const taskUiSchema = uiSchema;
 
@@ -30,7 +51,50 @@ const addLinkedPersonField = (schema :Object, uiSchema :Object) => {
   return { taskSchema, taskUiSchema };
 };
 
-/* eslint-disable import/prefer-default-export */
+const formatTasksForTable = (followUps :List, followUpNeighbors :Map) :Object[] => {
+
+  const sortedTasks :List = sortEntitiesByDateProperty(followUps || List(), [GENERAL_DATETIME]);
+  const tableData = [];
+  sortedTasks.forEach((task :Map) => {
+    const taskEKID :UUID = getEKID(task);
+    const {
+      [CATEGORY]: category,
+      [DATETIME_COMPLETED]: dateTimeCompleted,
+      [DESCRIPTION]: description,
+      [GENERAL_DATETIME]: dueDateTime,
+      [OL_TITLE]: title,
+      [STATUS]: status
+    } = getEntityProperties(task, [CATEGORY, DATETIME_COMPLETED, DESCRIPTION, GENERAL_DATETIME, OL_TITLE, STATUS]);
+    const person :Map = followUpNeighbors.getIn([taskEKID, SUBJECT_OF], Map());
+    const personName :string = getPersonFullName(person);
+    const taskName :string = getTaskNameForTaskManager(category, title, personName);
+    const dueDateString :string = `Due by: ${DateTime.fromISO(dueDateTime).toLocaleString(DateTime.DATE_SHORT)}`;
+    let taskStatus :string = status;
+    const today :DateTime = DateTime.local();
+    // $FlowFixMe
+    const dueDateIsBeforeToday :boolean = DateTime.fromISO(dueDateTime).startOf('day') < today.startOf('day');
+    if (status === FOLLOW_UPS_STATUSES.PENDING && !dateTimeCompleted && dueDateIsBeforeToday) {
+      taskStatus = FOLLOW_UPS_STATUSES.LATE;
+    }
+
+    let dateCompleted :string = '';
+    if (dateTimeCompleted) dateCompleted = DateTime.fromISO(dateTimeCompleted).toLocaleString(DateTime.DATE_SHORT);
+
+    const taskRow :Object = {
+      id: taskEKID,
+      taskName,
+      taskDescription: description,
+      dueDate: dueDateString,
+      taskStatus,
+      dateCompleted,
+      taskTitle: title,
+    };
+    tableData.push(taskRow);
+  });
+  return tableData;
+};
+
 export {
   addLinkedPersonField,
+  formatTasksForTable,
 };
