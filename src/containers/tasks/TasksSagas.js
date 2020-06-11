08a9c9fb00436c014fb7a04a615a3b957e28a5cd
Giulia Campana <giuliaecampana@gmail.com>
2020-06-10 17:39:20 -0700

    revise searchForTasks saga to make separate calls based on task status so that you can search for more than one status at once

--- src/containers/tasks/TasksSagas.js
+++ src/containers/tasks/TasksSagas.js
@@ -11,6 +11,7 @@ import {
   Map,
   fromJS,
 } from 'immutable';
+import { DateTime } from 'luxon';
 import {
   SearchApiActions,
   SearchApiSagas,
@@ -29,7 +30,7 @@ import {
   getNeighborESID,
   getPTIDFromEDM,
 } from '../../utils/DataUtils';
-import { getSearchTerm } from '../../utils/SearchUtils';
+import { getSearchTerm, getUTCDateRangeSearchString } from '../../utils/SearchUtils';
 import {
   GET_FOLLOW_UP_NEIGHBORS,
   LOAD_TASK_MANAGER_DATA,
@@ -55,7 +56,7 @@ const {
   PROVIDER,
   REENTRY_STAFF,
 } = APP_TYPE_FQNS;
-const { STATUS } = PROPERTY_TYPE_FQNS;
+const { GENERAL_DATETIME, STATUS } = PROPERTY_TYPE_FQNS;
 
 const getAppFromState = (state) => state.get(APP.APP, Map());
 const getEdmFromState = (state) => state.get(EDM.EDM, Map());
@@ -131,43 +132,108 @@ function* getFollowUpNeighborsWatcher() :Generator<*, *, *> {
 
 function* searchForTasksWorker(action :SequenceAction) :Generator<*, *, *> {
   const { id } = action;
+  let followUps :List = List();
 
   try {
     yield put(searchForTasks.request(id));
     const { value } = action;
     if (!isDefined(value)) throw ERR_ACTION_VALUE_NOT_DEFINED;
-    const { completed } = value;
+    const { statuses } = value;
 
-    const app = yield select(getAppFromState);
-    const edm = yield select(getEdmFromState);
-    const followUpsESID :UUID = getESIDFromApp(app, FOLLOW_UPS);
-    const statusPTID :UUID = getPTIDFromEDM(edm, STATUS);
+    if (isDefined(statuses) && statuses.length) {
+      const app = yield select(getAppFromState);
+      const edm = yield select(getEdmFromState);
+      const followUpsESID :UUID = getESIDFromApp(app, FOLLOW_UPS);
+      const statusPTID :UUID = getPTIDFromEDM(edm, STATUS);
 
-    const searchOptions = {
-      entitySetIds: [followUpsESID],
-      start: 0,
-      maxHits: 10000,
-      constraints: []
-    };
+      const searchOptionsForLateTasks = {
+        entitySetIds: [followUpsESID],
+        start: 0,
+        maxHits: 10000,
+        constraints: []
+      };
 
-    const searchString :string = completed ? FOLLOW_UPS_STATUSES.DONE : FOLLOW_UPS_STATUSES.PENDING;
-    const statusConstraint = getSearchTerm(statusPTID, searchString);
-    searchOptions.constraints.push({
-      min: 1,
-      constraints: [{
-        searchTerm: statusConstraint,
-        fuzzy: true
-      }]
-    });
-    const response :Object = yield call(executeSearchWorker, executeSearch({ searchOptions }));
-    if (response.error) throw response.error;
-    const followUps :List = fromJS(response.data.hits);
+      const searchOptionsForPendingTasks = {
+        entitySetIds: [followUpsESID],
+        start: 0,
+        maxHits: 10000,
+        constraints: []
+      };
 
-    if (response.data.numHits) {
-      const followUpEKIDs :UUID[] = [];
-      followUps.forEach((followUp :Map) => followUpEKIDs.push(getEKID(followUp)));
+      const searchOptionsForCompletedTasks = {
+        entitySetIds: [followUpsESID],
+        start: 0,
+        maxHits: 10000,
+        constraints: []
+      };
 
-      yield call(getFollowUpNeighborsWorker, getFollowUpNeighbors({ followUpEKIDs }));
+      const searchCalls = [];
+
+      statuses.forEach((status :string) => {
+        let searchString :string = status;
+        let searchOptions = {};
+
+        const yesterday = DateTime.local().minus({ days: 1 }).endOf('day');
+        const dueDatePTID :UUID = getPTIDFromEDM(edm, GENERAL_DATETIME);
+
+        if (status === FOLLOW_UPS_STATUSES.LATE) {
+          searchString = FOLLOW_UPS_STATUSES.PENDING;
+          searchOptions = searchOptionsForLateTasks;
+          const datetimeSearchTerm = getUTCDateRangeSearchString(dueDatePTID, 'day', undefined, yesterday);
+          searchOptions.constraints.push({
+            min: 1,
+            constraints: [{
+              searchTerm: datetimeSearchTerm,
+              fuzzy: false
+            }]
+          });
+          searchCalls.push(call(executeSearchWorker, executeSearch({ searchOptions })));
+        }
+
+        if (status === FOLLOW_UPS_STATUSES.PENDING) {
+          searchOptions = searchOptionsForPendingTasks;
+          const datetimeSearchTerm = getUTCDateRangeSearchString(dueDatePTID, 'day', yesterday, undefined);
+          searchOptionsForPendingTasks.constraints.push({
+            min: 1,
+            constraints: [{
+              searchTerm: datetimeSearchTerm,
+              fuzzy: false
+            }]
+          });
+          searchCalls.push(call(executeSearchWorker, executeSearch({ searchOptions })));
+        }
+
+        if (status === FOLLOW_UPS_STATUSES.DONE) {
+          searchOptions = searchOptionsForCompletedTasks;
+          searchCalls.push(call(executeSearchWorker, executeSearch({ searchOptions })));
+        }
+
+        const statusConstraint = getSearchTerm(statusPTID, searchString);
+        searchOptions.constraints.push({
+          min: 1,
+          constraints: [{
+            searchTerm: statusConstraint,
+            fuzzy: true
+          }]
+        });
+      });
+
+      const responses :Object[] = yield all(searchCalls);
+      const responseError = responses.reduce(
+        (error :any, r :Object) => (isDefined(error) ? error : r.error),
+        undefined,
+      );
+      if (responseError) throw responseError;
+      responses.forEach((response :Object) => {
+        followUps = followUps.concat(fromJS(response.data.hits));
+      });
+
+      if (!followUps.isEmpty()) {
+        const followUpEKIDs :UUID[] = [];
+        followUps.forEach((followUp :Map) => followUpEKIDs.push(getEKID(followUp)));
+
+        yield call(getFollowUpNeighborsWorker, getFollowUpNeighbors({ followUpEKIDs }));
+      }
     }
 
     yield put(searchForTasks.success(id, followUps));
@@ -199,7 +265,7 @@ function* loadTaskManagerDataWorker(action :SequenceAction) :Generator<*, *, *> 
     yield put(loadTaskManagerData.request(id));
 
     yield all([
-      call(searchForTasksWorker, searchForTasks({ completed: false })),
+      call(searchForTasksWorker, searchForTasks({ statuses: [] })),
       call(getEntitiesForNewFollowUpFormWorker, getEntitiesForNewFollowUpForm()),
     ]);
 
