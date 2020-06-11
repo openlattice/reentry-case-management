4cac4ce81031d5ada75082b95b06ea50c053c773
Giulia Campana <giuliaecampana@gmail.com>
2020-06-11 00:49:25 -0700

    add more education levels

--- src/containers/intake/schemas/IntakeSchemas.js
+++ src/containers/intake/schemas/IntakeSchemas.js
@@ -5,7 +5,6 @@ import { DateTime } from 'luxon';
 import { generateReviewSchemas } from '../../../utils/FormUtils';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 import {
-  COUNTRIES,
   EDUCATION_LEVELS,
   ETHNICITIES,
   GENDERS,
