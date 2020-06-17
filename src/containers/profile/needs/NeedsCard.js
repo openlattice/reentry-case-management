294573dba62d113ff59a850049a81fa11eba2d23
Giulia Campana <giuliaecampana@gmail.com>
2020-06-17 14:07:41 -0700

    put card header with buttons in styles file

--- src/containers/profile/needs/NeedsCard.js
+++ src/containers/profile/needs/NeedsCard.js
@@ -4,25 +4,19 @@ import styled from 'styled-components';
 import { Map } from 'immutable';
 import {
   Card,
-  CardHeader,
   CardSegment,
   Colors,
   EditButton,
 } from 'lattice-ui-kit';
 
 import EditNeedsModal from './EditNeedsModal';
-import { SmallCardHeaderTitle } from '../styled/GeneralProfileStyles';
+import { CardHeaderWithButtons, SmallCardHeaderTitle } from '../styled/GeneralProfileStyles';
 import COLORS from '../../../core/style/Colors';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
 const { NEUTRALS } = Colors;
 const { NEEDS_ASSESSMENT } = APP_TYPE_FQNS;
 const { NOTES, TYPE } = PROPERTY_TYPE_FQNS;
-
-const NeedsCardHeader = styled(CardHeader)`
-  align-items: center;
-  justify-content: space-between;
-`;
 
 const NeedsTag = styled.div`
   background-color: ${NEUTRALS[6]};
@@ -49,10 +43,10 @@ const NeedsCard = ({ participantNeighbors } :Props) => {
   const notes :string = participantNeighbors.getIn([NEEDS_ASSESSMENT, 0, NOTES, 0], '');
   return (
     <Card>
-      <NeedsCardHeader padding="30px" vertical={false}>
+      <CardHeaderWithButtons padding="30px" vertical={false}>
         <SmallCardHeaderTitle>Needs</SmallCardHeaderTitle>
         <EditButton onClick={() => setEditModalVisibility(true)}>Edit</EditButton>
-      </NeedsCardHeader>
+      </CardHeaderWithButtons>
       {
         needs && (
           <CardSegment padding="30px" vertical={false}>
