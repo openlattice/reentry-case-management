0c6be6b18f7475fc1e2299d923a2727a339e327d
Giulia Campana <giuliaecampana@gmail.com>
2020-06-19 08:58:33 -0700

    fix enrollment date schema

--- src/containers/profile/programhistory/schemas/EditEnrollmentDateSchemas.js
+++ src/containers/profile/programhistory/schemas/EditEnrollmentDateSchemas.js
@@ -5,8 +5,8 @@ import { DateTime } from 'luxon';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../../core/edm/constants/FullyQualifiedNames';
 
 const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
-const { ENROLLMENT_STATUS } = APP_TYPE_FQNS;
-const { EFFECTIVE_DATE } = PROPERTY_TYPE_FQNS;
+const { NEEDS_ASSESSMENT } = APP_TYPE_FQNS;
+const { DATETIME_COMPLETED } = PROPERTY_TYPE_FQNS;
 
 const schema :Object = {
   type: 'object',
@@ -16,7 +16,7 @@ const schema :Object = {
       type: 'object',
       title: '',
       properties: {
-        [getEntityAddressKey(0, ENROLLMENT_STATUS, EFFECTIVE_DATE)]: {
+        [getEntityAddressKey(0, NEEDS_ASSESSMENT, DATETIME_COMPLETED)]: {
           type: 'string',
           title: 'Re-entry enrollment date',
           format: 'date',
@@ -30,7 +30,7 @@ const schema :Object = {
 const uiSchema :Object = {
   [getPageSectionKey(1, 1)]: {
     classNames: 'column-span-12 grid-container',
-    [getEntityAddressKey(0, ENROLLMENT_STATUS, EFFECTIVE_DATE)]: {
+    [getEntityAddressKey(0, NEEDS_ASSESSMENT, DATETIME_COMPLETED)]: {
       classNames: 'column-span-12',
     },
   }
