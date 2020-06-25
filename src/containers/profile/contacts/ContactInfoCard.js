0f56f2276fd80f46dcbb77b014977a6927b50ad3
Giulia Campana <giuliaecampana@gmail.com>
2020-06-24 23:56:56 -0700

    add emergency contact section

--- src/containers/profile/contacts/ContactInfoCard.js
+++ src/containers/profile/contacts/ContactInfoCard.js
@@ -1,28 +1,54 @@
 // @flow
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { List, Map } from 'immutable';
 import {
+  Button,
   Card,
   CardSegment,
   DataGrid,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
   EditButton,
   Label,
 } from 'lattice-ui-kit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/pro-light-svg-icons';
 
 import EditContactInfoModal from './EditContactInfoModal';
+import EditEmergencyContactsModal from './EditEmergencyContactsModal';
 import { CardHeaderWithButtons, SmallCardHeaderTitle } from '../styled/GeneralProfileStyles';
 import { getAddress, getPersonContactData } from '../utils/ContactsUtils';
 import { EMPTY_FIELD } from '../../../utils/constants/GeneralConstants';
 import { APP_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
 const { LOCATION } = APP_TYPE_FQNS;
-
-const labelMap = Map({
+const expandIcon = <FontAwesomeIcon icon={faChevronDown} size="xs" />;
+const personLabelMap = Map({
   phone: 'Phone number',
   email: 'Email',
   preferredMethod: 'Preferred method',
   preferredTime: 'Preferred time',
 });
+
+const AddressWrapper = styled.div`
+  margin: 30px 0;
+`;
+
+const LabelRow = styled.div`
+  display: grid;
+  flex: 1;
+  grid-auto-flow: row;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  grid-gap: 20px 30px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
 
 type Props = {
   participantNeighbors :Map;
@@ -31,6 +57,7 @@ type Props = {
 const ContactInfoCard = ({ participantNeighbors } :Props) => {
 
   const [editModalVisible, setEditModalVisibility] = useState(false);
+  const [editEmergencyModalVisible, setEditEmergencyVisibility] = useState(false);
   const contactData :Map = getPersonContactData(participantNeighbors);
   const addressList :List = participantNeighbors.get(LOCATION, List());
   const address :Map = addressList.get(0);
@@ -46,14 +73,40 @@ const ContactInfoCard = ({ participantNeighbors } :Props) => {
         <DataGrid
             data={contactData}
             emptyString={EMPTY_FIELD}
-            labelMap={labelMap}
+            labelMap={personLabelMap}
             truncate />
-        <Label subtle>Address</Label>
-        <div>{ addressString }</div>
+        <AddressWrapper>
+          <Label subtle>Address</Label>
+          <div>{ addressString }</div>
+        </AddressWrapper>
+        <div>
+          <ExpansionPanel>
+            <ExpansionPanelSummary expandIcon={expandIcon}>
+              <div>Emergency Contacts</div>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <CardSegment padding="0">
+                <LabelRow>
+                  <Label subtle>Name</Label>
+                  <Label subtle>Phone</Label>
+                  <Label subtle>Email</Label>
+                  <Label subtle>Relationship</Label>
+                </LabelRow>
+                <ButtonRow>
+                  <Button onClick={() => setEditEmergencyVisibility(true)}>Edit</Button>
+                </ButtonRow>
+              </CardSegment>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
       </CardSegment>
       <EditContactInfoModal
           isVisible={editModalVisible}
           onClose={() => setEditModalVisibility(false)}
+          participantNeighbors={participantNeighbors} />
+      <EditEmergencyContactsModal
+          isVisible={editEmergencyModalVisible}
+          onClose={() => setEditEmergencyVisibility(false)}
           participantNeighbors={participantNeighbors} />
     </Card>
   );
