b79e692b347eb1ce3f0ab557decc802079395f3b
Giulia Campana <giuliaecampana@gmail.com>
2020-06-26 07:45:05 -0700

    get modal working for edit

--- src/containers/profile/court/EditCourtDatesModal.js
+++ src/containers/profile/court/EditCourtDatesModal.js
@@ -1,7 +1,12 @@
 // @flow
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { List, Map, setIn } from 'immutable';
+import {
+  List,
+  Map,
+  mergeDeep,
+  setIn,
+} from 'immutable';
 import { Modal, ModalFooter } from 'lattice-ui-kit';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,12 +15,37 @@ import ModalHeader from '../../../components/modal/ModalHeader';
 import { getEKID } from '../../../utils/DataUtils';
 import { clearEditRequestState } from '../needs/NeedsActions';
 import { schema, uiSchema } from './schemas/EditCourtDatesSchemas';
+import {
+  getCourtHearingAssociations,
+  getHearingsEntityIndexToIdMap,
+  preprocessEditedCourtData,
+  preprocessNewCourtData,
+} from '../utils/CourtUtils';
+import { requestIsPending, requestIsSuccess } from '../../../utils/RequestStateUtils';
+import { EDIT_COURT_HEARINGS, editCourtHearings } from './CourtActions';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { PROFILE } from '../../../utils/constants/ReduxStateConstants';
+import {
+  APP,
+  EDM,
+  PROFILE,
+  SHARED
+} from '../../../utils/constants/ReduxStateConstants';
 
-const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
+const {
+  findEntityAddressKeyFromMap,
+  getEntityAddressKey,
+  getPageSectionKey,
+  processAssociationEntityData,
+  processEntityData,
+  processEntityDataForPartialReplace,
+  replaceEntityAddressKeys,
+  wrapFormDataInPageSection,
+} = DataProcessingUtils;
 const { HEARINGS } = APP_TYPE_FQNS;
 const { DATE, TYPE } = PROPERTY_TYPE_FQNS;
+const { ACTIONS, REQUEST_STATE } = SHARED;
+const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
+const { TYPE_IDS_BY_FQN, PROPERTY_TYPES } = EDM;
 
 const InnerWrapper = styled.div`
   padding-top: 30px;
@@ -33,18 +63,20 @@ const EditCourtDatesModal = ({
   participantNeighbors,
 } :Props) => {
 
+  const entityIndexToIdMap :Map = getHearingsEntityIndexToIdMap(participantNeighbors);
+
   let originalFormData = {
     [getPageSectionKey(1, 1)]: []
   };
   participantNeighbors.get(HEARINGS, List()).forEach((hearing :Map, index :number) => {
     originalFormData = setIn(
       originalFormData,
-      [getPageSectionKey(1, 1), index, getEntityAddressKey(0, HEARINGS, DATE)],
+      [getPageSectionKey(1, 1), index, getEntityAddressKey(-1, HEARINGS, DATE)],
       hearing.getIn([DATE, 0])
     );
     originalFormData = setIn(
       originalFormData,
-      [getPageSectionKey(1, 1), index, getEntityAddressKey(0, HEARINGS, TYPE)],
+      [getPageSectionKey(1, 1), index, getEntityAddressKey(-1, HEARINGS, TYPE)],
       hearing.getIn([TYPE, 0])
     );
   });
@@ -52,24 +84,106 @@ const EditCourtDatesModal = ({
   const [formData, updateFormData] = useState(originalFormData);
   const dispatch = useDispatch();
 
+  const selectedOrgId :string = useSelector((store :Map) => store.getIn([APP.APP, SELECTED_ORG_ID]));
+  const entitySetIds :Map = useSelector((store :Map) => store.getIn([
+    APP.APP,
+    ENTITY_SET_IDS_BY_ORG_ID,
+    selectedOrgId
+  ], Map()));
+  const propertyTypeIds :Map = useSelector((store :Map) => store.getIn([
+    EDM.EDM,
+    TYPE_IDS_BY_FQN,
+    PROPERTY_TYPES
+  ], Map()));
   const participant :Map = useSelector((store :Map) => store.getIn([PROFILE.PROFILE, PROFILE.PARTICIPANT], Map()));
   const personEKID :UUID = getEKID(participant);
+  const editCourtHearingsReqState = useSelector((store :Map) => store.getIn([
+    PROFILE.PROFILE,
+    ACTIONS,
+    EDIT_COURT_HEARINGS,
+    REQUEST_STATE
+  ]));
 
   const closeModal = useCallback(() => {
     dispatch(clearEditRequestState());
     onClose();
   }, [dispatch, onClose]);
 
+  useEffect(() => {
+    if (requestIsSuccess(editCourtHearingsReqState)) {
+      closeModal();
+    }
+  }, [closeModal, editCourtHearingsReqState]);
+
   const onChange = ({ formData: newFormData } :Object) => {
     updateFormData(newFormData);
   };
 
-  const onSubmit = () => {};
+  const onSubmit = () => {
+    let hearingsDataToSubmit :Object = {};
+    let hearingsAssociations :Object = {};
+
+    const formDataWithCourtDataOnly :Object = preprocessNewCourtData(formData, originalFormData);
+
+    if (formDataWithCourtDataOnly[getPageSectionKey(1, 1)].length) {
+
+      hearingsDataToSubmit = processEntityData(
+        formDataWithCourtDataOnly,
+        entitySetIds,
+        propertyTypeIds,
+      );
+      const associations = getCourtHearingAssociations(formDataWithCourtDataOnly, personEKID);
+      hearingsAssociations = processAssociationEntityData(
+        associations,
+        entitySetIds,
+        propertyTypeIds,
+      );
+    }
+
+    const { editedCourtDataAsImmutable, originalCourtDataAsImmutable } = preprocessEditedCourtData(
+      formData,
+      originalFormData,
+      formDataWithCourtDataOnly
+    );
+
+    let hearingsDataToEdit :Object = {};
+
+    editedCourtDataAsImmutable.forEach((hearing :Map, index :number) => {
+      const hearingDraftWithKeys :Map = replaceEntityAddressKeys(
+        hearing,
+        findEntityAddressKeyFromMap(entityIndexToIdMap, index)
+      );
+      const originalContactWithKeys :Map = replaceEntityAddressKeys(
+        originalCourtDataAsImmutable.get(index),
+        findEntityAddressKeyFromMap(entityIndexToIdMap, index)
+      );
+      const hearingsDataDiff = processEntityDataForPartialReplace(
+        wrapFormDataInPageSection(hearingDraftWithKeys),
+        wrapFormDataInPageSection(originalContactWithKeys),
+        entitySetIds,
+        propertyTypeIds,
+        {}
+      );
+      hearingsDataToEdit = mergeDeep(hearingsDataToEdit, hearingsDataDiff);
+    });
+
+    if (Object.values(hearingsDataToSubmit).length || Object.values(hearingsDataToEdit).length) {
+      dispatch(editCourtHearings({
+        hearingsAssociations,
+        hearingsDataToEdit,
+        hearingsDataToSubmit,
+        participantNeighbors,
+      }));
+    }
+    else {
+      onClose();
+    }
+  };
 
   const renderHeader = () => (<ModalHeader onClose={onClose} title="Edit Court Hearings" />);
   const withFooter = (
     <ModalFooter
-        isPendingPrimary={false}
+        isPendingPrimary={requestIsPending(editCourtHearingsReqState)}
         onClickPrimary={onSubmit}
         onClickSecondary={closeModal}
         shouldStretchButtons
