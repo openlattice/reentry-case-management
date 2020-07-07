6eb151a44147a759a76be1ccc56eb4543d4225a9
Giulia Campana <giuliaecampana@gmail.com>
2020-07-06 18:20:48 -0700

    add submission workflow to EditPersonForm

--- src/containers/profile/person/EditPersonForm.js
+++ src/containers/profile/person/EditPersonForm.js
@@ -1,14 +1,25 @@
 // @flow
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
-import { Map, getIn } from 'immutable';
+import { Map } from 'immutable';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Card, CardSegment, Spinner } from 'lattice-ui-kit';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import type { RequestSequence, RequestState } from 'redux-reqseq';
 
+import { EDIT_PERSON, editPerson } from './EditPersonActions';
 import { personSchema, personUiSchema } from './schemas/EditPersonSchemas';
 
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { getEntityProperties } from '../../../utils/DataUtils';
-import { isDefined } from '../../../utils/LangUtils';
+import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
+import { requestIsPending } from '../../../utils/RequestStateUtils';
+import {
+  APP,
+  EDM,
+  PROFILE,
+  SHARED
+} from '../../../utils/constants/ReduxStateConstants';
 
 const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
 const { PEOPLE } = APP_TYPE_FQNS;
@@ -22,12 +33,30 @@ const {
   PERSON_SEX,
   RACE,
 } = PROPERTY_TYPE_FQNS;
+const { ACTIONS, REQUEST_STATE } = SHARED;
+const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
+const { PROPERTY_TYPES, TYPE_IDS_BY_FQN } = EDM;
 
 type Props = {
+  actions :{
+    editPerson :RequestSequence;
+  };
+  entitySetIds :Map;
   participant :Map;
+  propertyTypeIds :Map;
+  requestStates :{
+    EDIT_PERSON :RequestState;
+  };
 };
 
-const EditPersonForm = ({ participant } :Props) => {
+const EditPersonForm = ({
+  actions,
+  entitySetIds,
+  participant,
+  propertyTypeIds,
+  requestStates,
+} :Props) => {
+
   const {
     [COUNTY_ID]: countyID,
     [DOB]: dobISO,
@@ -54,27 +83,60 @@ const EditPersonForm = ({ participant } :Props) => {
       [getEntityAddressKey(0, PEOPLE, ETHNICITY)]: ethnicity,
     }
   };
-  const [formData, updateFormData] = useState({});
+  const [formData, updateFormData] = useState(originalFormData);
   const onChange = ({ formData: newFormData } :Object) => {
     updateFormData(newFormData);
   };
 
-  useEffect(() => {
-    const formLastName = getIn(formData, [getPageSectionKey(1, 1), getEntityAddressKey(0, PEOPLE, LAST_NAME)]);
-    if (!isDefined(formLastName) && isDefined(participant)) {
-      onChange({ formData: originalFormData });
-    }
-  }, [formData, originalFormData, participant]);
-  const onSubmit = () => {};
+  const personEKID :UUID = getEKID(participant);
+  const entityIndexToIdMap :Map = Map().withMutations((map :Map) => {
+    map.setIn([PEOPLE, 0], personEKID);
+  });
+
+  const formContext = {
+    editAction: actions.editPerson,
+    entityIndexToIdMap,
+    entitySetIds,
+    propertyTypeIds,
+  };
+
+  if (requestIsPending(requestStates[EDIT_PERSON])) {
+    return (
+      <Card>
+        <CardSegment vertical>
+          <Spinner size="2x" />
+        </CardSegment>
+      </Card>
+    );
+  }
+
   return (
     <Form
         disabled
+        formContext={formContext}
         formData={formData}
         onChange={onChange}
-        onSubmit={onSubmit}
         schema={personSchema}
         uiSchema={personUiSchema} />
   );
 };
 
-export default EditPersonForm;
+const mapStateToProps = (state :Map) => {
+  const selectedOrgId = state.getIn([APP.APP, SELECTED_ORG_ID], '');
+  return {
+    entitySetIds: state.getIn([APP.APP, ENTITY_SET_IDS_BY_ORG_ID, selectedOrgId], Map()),
+    propertyTypeIds: state.getIn([EDM.EDM, TYPE_IDS_BY_FQN, PROPERTY_TYPES], Map()),
+    requestStates: {
+      [EDIT_PERSON]: state.getIn([PROFILE.PROFILE, ACTIONS, EDIT_PERSON, REQUEST_STATE]),
+    }
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators({
+    editPerson
+  }, dispatch)
+});
+
+// $FlowFixMe
+export default connect(mapStateToProps, mapDispatchToProps)(EditPersonForm);
