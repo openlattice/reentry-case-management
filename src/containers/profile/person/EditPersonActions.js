3d799c7ee377412e762226a9f0bc577a00693187
Giulia Campana <giuliaecampana@gmail.com>
2020-07-08 18:58:39 -0700

    sort actions

--- src/containers/profile/person/EditPersonActions.js
+++ src/containers/profile/person/EditPersonActions.js
@@ -5,20 +5,20 @@ import type { RequestSequence } from 'redux-reqseq';
 const EDIT_EDUCATION :'EDIT_EDUCATION' = 'EDIT_EDUCATION';
 const editEducation :RequestSequence = newRequestSequence(EDIT_EDUCATION);
 
-const SUBMIT_EDUCATION :'SUBMIT_EDUCATION' = 'SUBMIT_EDUCATION';
-const submitEducation :RequestSequence = newRequestSequence(SUBMIT_EDUCATION);
-
 const EDIT_PERSON :'EDIT_PERSON' = 'EDIT_PERSON';
 const editPerson :RequestSequence = newRequestSequence(EDIT_PERSON);
 
 const EDIT_PERSON_DETAILS :'EDIT_PERSON_DETAILS' = 'EDIT_PERSON_DETAILS';
 const editPersonDetails :RequestSequence = newRequestSequence(EDIT_PERSON_DETAILS);
 
-const SUBMIT_PERSON_DETAILS :'SUBMIT_PERSON_DETAILS' = 'SUBMIT_PERSON_DETAILS';
-const submitPersonDetails :RequestSequence = newRequestSequence(SUBMIT_PERSON_DETAILS);
-
 const EDIT_STATE_ID :'EDIT_STATE_ID' = 'EDIT_STATE_ID';
 const editStateId :RequestSequence = newRequestSequence(EDIT_STATE_ID);
+
+const SUBMIT_EDUCATION :'SUBMIT_EDUCATION' = 'SUBMIT_EDUCATION';
+const submitEducation :RequestSequence = newRequestSequence(SUBMIT_EDUCATION);
+
+const SUBMIT_PERSON_DETAILS :'SUBMIT_PERSON_DETAILS' = 'SUBMIT_PERSON_DETAILS';
+const submitPersonDetails :RequestSequence = newRequestSequence(SUBMIT_PERSON_DETAILS);
 
 const SUBMIT_STATE_ID :'SUBMIT_STATE_ID' = 'SUBMIT_STATE_ID';
 const submitStateId :RequestSequence = newRequestSequence(SUBMIT_STATE_ID);
