efedcf6a6e107973029eec406fa4aacffeac3564
Giulia Campana <giuliaecampana@gmail.com>
2020-07-16 16:39:41 -0700

    udpate styles of ContactSupportButton

--- src/components/buttons/ContactSupportButton.js
+++ src/components/buttons/ContactSupportButton.js
@@ -4,31 +4,30 @@ import React from 'react';
 import styled from 'styled-components';
 import { faQuestionCircle } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Colors } from 'lattice-ui-kit';
+import { Colors } from 'lattice-ui-kit';
 
-const { NEUTRALS } = Colors;
+const { NEUTRAL, WHITE } = Colors;
 
-const SupportButton = styled(Button)`
-  background-color: ${NEUTRALS[7]};
-  border-color: ${NEUTRALS[7]};
+const SupportButton = styled.a`
+  background-color: ${WHITE};
   border-radius: 3px;
+  border: 1px solid ${NEUTRAL.N700};
   bottom: 30px;
-  color: ${NEUTRALS[0]};
+  color: ${NEUTRAL.N700};
+  display: flex;
+  font-size: 12px;
+  font-weight: 600;
   height: 37px;
   padding: 10px 20px;
+  place-items: center;
   position: fixed;
   right: 16px;
   text-decoration: none;
-  width: 200px;
-
-  span {
-    font-size: 14px;
-    font-weight: 600;
-  }
+  width: 165px;
 `;
 
 const QuestionMarkIcon = styled(FontAwesomeIcon).attrs(() => ({
-  color: NEUTRALS[1],
+  color: NEUTRAL.N700,
   icon: faQuestionCircle,
   fixedWidth: true,
 }))`
