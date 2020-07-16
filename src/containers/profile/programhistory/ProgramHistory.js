7bf115c25be535d352e3782ae734942fd90dcd5f
Giulia Campana <giuliaecampana@gmail.com>
2020-07-16 15:04:45 -0700

    use new EditButton in all files where there is an EditButton

--- src/containers/profile/programhistory/ProgramHistory.js
+++ src/containers/profile/programhistory/ProgramHistory.js
@@ -1,17 +1,24 @@
 // @flow
 import React, { useState } from 'react';
+
 import styled from 'styled-components';
 import { List, Map } from 'immutable';
 import {
   Card,
   CardSegment,
   Colors,
-  EditButton,
 } from 'lattice-ui-kit';
 
-import Event from './Event';
 import EditEventModal from './EditEventModal';
 import EditReleaseInfoModal from './EditReleaseInfoModal';
+import Event from './Event';
+import { schema, uiSchema } from './schemas/EditEnrollmentDateSchemas';
+
+import EditButton from '../../../components/buttons/EditButton';
+import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { getEKID } from '../../../utils/DataUtils';
+import { sortEntitiesByDateProperty } from '../../../utils/Utils';
+import { EMPTY_FIELD } from '../../../utils/constants/GeneralConstants';
 import {
   CardInnerWrapper,
   EventDateWrapper,
@@ -20,12 +27,7 @@ import {
   EventWrapper,
 } from '../styled/EventStyles';
 import { CardHeaderWithButtons, SmallCardHeaderTitle } from '../styled/GeneralProfileStyles';
-import { getEKID } from '../../../utils/DataUtils';
-import { sortEntitiesByDateProperty } from '../../../utils/Utils';
 import { getMostRecentReleaseDate, getReentryEnrollmentDate } from '../utils/ProfileUtils';
-import { schema, uiSchema } from './schemas/EditEnrollmentDateSchemas';
-import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { EMPTY_FIELD } from '../../../utils/constants/GeneralConstants';
 
 const { NEUTRALS } = Colors;
 const {
