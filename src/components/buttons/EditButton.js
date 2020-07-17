777737439cacfc90b2851d4168ddd5709e3ff3ca
Giulia Campana <giuliaecampana@gmail.com>
2020-07-17 12:09:41 -0700

    update EditButton

--- src/components/buttons/EditButton.js
+++ src/components/buttons/EditButton.js
@@ -1,13 +1,19 @@
 // @flow
 import React from 'react';
 
+import styled from 'styled-components';
 import { faPen } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Colors, IconButton } from 'lattice-ui-kit';
+import { Button, Colors } from 'lattice-ui-kit';
 
 const { NEUTRAL } = Colors;
 
+const IconMarginRight = styled.span`
+  margin: 0 8px 0 0;
+`;
+
 type Props = {
+  children :any;
   onClick :() => void;
 };
 
@@ -15,8 +21,20 @@ const EditIcon = (
   <FontAwesomeIcon color={NEUTRAL.N700} icon={faPen} />
 );
 
-const EditButton = ({ onClick } :Props) => (
-  <IconButton icon={EditIcon} onClick={onClick} />
-);
+const EditButton = ({ children, onClick } :Props) => {
+
+  if (!children) {
+    return (
+      <Button onClick={onClick}>{EditIcon}</Button>
+    );
+  }
+
+  return (
+    <Button onClick={onClick}>
+      <IconMarginRight>{EditIcon}</IconMarginRight>
+      {children}
+    </Button>
+  );
+};
 
 export default EditButton;
