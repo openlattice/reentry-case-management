0916a2f768206bf06ad6fa259fbc97a66d8b4f9d
Giulia Campana <giuliaecampana@gmail.com>
2020-07-17 15:02:31 -0700

    use Button for ContactSupportButton and create a button wrapper

--- src/components/buttons/ContactSupportButton.js
+++ src/components/buttons/ContactSupportButton.js
@@ -4,30 +4,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { faQuestionCircle } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Colors } from 'lattice-ui-kit';
+import { Button, Colors } from 'lattice-ui-kit';
 
-const { NEUTRAL, WHITE } = Colors;
+const { NEUTRAL } = Colors;
 
-const SupportButton = styled.a`
-  background-color: ${WHITE};
-  border-radius: 3px;
-  border: 1px solid ${NEUTRAL.N700};
-  bottom: 30px;
-  color: ${NEUTRAL.N700};
-  display: flex;
-  font-size: 12px;
-  font-weight: 600;
-  height: 37px;
-  padding: 10px 20px;
-  place-items: center;
+const ButtonWrapper = styled.div`
+  bottom: 16px;
   position: fixed;
   right: 16px;
-  text-decoration: none;
-  width: 165px;
+  width: 200px;
 `;
 
 const QuestionMarkIcon = styled(FontAwesomeIcon).attrs(() => ({
-  color: NEUTRAL.N700,
+  color: NEUTRAL.N900,
   icon: faQuestionCircle,
   fixedWidth: true,
 }))`
@@ -36,10 +25,15 @@ const QuestionMarkIcon = styled(FontAwesomeIcon).attrs(() => ({
 `;
 
 const ContactSupportButton = () => (
-  <SupportButton href="https://support.openlattice.com/servicedesk/customer/portal/1" target="_blank">
-    <QuestionMarkIcon />
-    <span>Contact Support</span>
-  </SupportButton>
+  <ButtonWrapper>
+    <Button
+        href="https://support.openlattice.com/servicedesk/customer/portal/1"
+        target="_blank"
+        variant="outlined">
+      <QuestionMarkIcon />
+      <span>Contact Support</span>
+    </Button>
+  </ButtonWrapper>
 );
 
 export default ContactSupportButton;
