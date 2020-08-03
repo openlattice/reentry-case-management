2aea1df3dfa1c5da3ae9096cbfc9b3e93d3fa5d4
Giulia Campana <giuliaecampana@gmail.com>
2020-08-03 10:04:36 -0500

    validation util for phone numbers

--- src/containers/profile/utils/PhoneNumberUtils.js
+++ src/containers/profile/utils/PhoneNumberUtils.js
@@ -1,7 +1,7 @@
 // @flow
 import { getIn, setIn } from 'immutable';
 import { DataProcessingUtils } from 'lattice-fabricate';
-import { AsYouType } from 'libphonenumber-js';
+import { AsYouType, parsePhoneNumberFromString } from 'libphonenumber-js';
 
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 
@@ -29,6 +29,7 @@ const formatPhoneNumbersAsYouType = (
   const cellPhoneInput = getIn(formDataWithPhoneNumbersFormatted, cellPhoneKey) || defaultValue;
 
   if (homePhoneInput) {
+    // https://github.com/catamphetamine/libphonenumber-js/issues/225
     const homePhone = homePhoneInput.length <= 4 ? homePhoneInput : new AsYouType('US').input(homePhoneInput);
     formDataWithPhoneNumbersFormatted = setIn(formDataWithPhoneNumbersFormatted, homePhoneKey, homePhone);
   }
@@ -40,7 +41,33 @@ const formatPhoneNumbersAsYouType = (
   return formDataWithPhoneNumbersFormatted;
 };
 
-/* eslint-disable import/prefer-default-export */
+const validateParticipantPhoneNumbers = (formData :Object, errors :Object) => {
+  const pageSection1 = getPageSectionKey(1, 2);
+  const homePhoneKey = getEntityAddressKey(0, CONTACT_INFO, PHONE_NUMBER);
+  const cellPhoneKey = getEntityAddressKey(1, CONTACT_INFO, PHONE_NUMBER);
+
+  const errorMessage :string = 'Phone number is invalid';
+
+  const homePhone :string = getIn(formData, [pageSection1, homePhoneKey]);
+  if (homePhone) {
+    const parsedHomePhone = parsePhoneNumberFromString(homePhone, 'US');
+    if (!parsedHomePhone.isPossible()) {
+      errors[pageSection1][homePhoneKey].addError(errorMessage);
+    }
+  }
+
+  const cellPhone :string = getIn(formData, [pageSection1, cellPhoneKey]);
+  if (cellPhone) {
+    const parsedCellPhone = parsePhoneNumberFromString(cellPhone, 'US');
+    if (!parsedCellPhone.isPossible()) {
+      errors[pageSection1][cellPhoneKey].addError(errorMessage);
+    }
+  }
+
+  return errors;
+};
+
 export {
   formatPhoneNumbersAsYouType,
+  validateParticipantPhoneNumbers,
 };
