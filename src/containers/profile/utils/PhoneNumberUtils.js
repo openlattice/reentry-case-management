110f422ec8da9474811410d096e0506a5ced91d1
Giulia Campana <giuliaecampana@gmail.com>
2020-08-03 15:33:17 -0500

    add attorney and officer phone numbers to PhoneNumberUtils

--- src/containers/profile/utils/PhoneNumberUtils.js
+++ src/containers/profile/utils/PhoneNumberUtils.js
@@ -28,6 +28,19 @@ const formatPhoneNumbersAsYouType = (
   ];
   const cellPhoneInput = getIn(formDataWithPhoneNumbersFormatted, cellPhoneKey) || defaultValue;
 
+  const attorneyPhoneKey :string[] = [
+    getPageSectionKey(1, 5),
+    getPageSectionKey(1, 6),
+    getEntityAddressKey(-2, CONTACT_INFO, PHONE_NUMBER)
+  ];
+  const attorneyPhoneInput = getIn(formDataWithPhoneNumbersFormatted, attorneyPhoneKey) || defaultValue;
+  const probationOfficerPhoneKey :string[] = [
+    getPageSectionKey(1, 5),
+    getPageSectionKey(1, 6),
+    getEntityAddressKey(-4, CONTACT_INFO, PHONE_NUMBER)
+  ];
+  const probationOfficerPhoneInput = getIn(formDataWithPhoneNumbersFormatted, probationOfficerPhoneKey) || defaultValue;
+
   if (homePhoneInput) {
     // https://github.com/catamphetamine/libphonenumber-js/issues/225
     const homePhone = homePhoneInput.length <= 4 ? homePhoneInput : new AsYouType('US').input(homePhoneInput);
@@ -37,30 +50,68 @@ const formatPhoneNumbersAsYouType = (
     const cellPhone = cellPhoneInput.length <= 4 ? cellPhoneInput : new AsYouType('US').input(cellPhoneInput);
     formDataWithPhoneNumbersFormatted = setIn(formDataWithPhoneNumbersFormatted, cellPhoneKey, cellPhone);
   }
+  if (attorneyPhoneInput) {
+    const attorneyPhone = attorneyPhoneInput.length <= 4
+      ? attorneyPhoneInput
+      : new AsYouType('US').input(attorneyPhoneInput);
+    formDataWithPhoneNumbersFormatted = setIn(formDataWithPhoneNumbersFormatted, attorneyPhoneKey, attorneyPhone);
+  }
+  if (probationOfficerPhoneInput) {
+    const probationOfficerPhone = probationOfficerPhoneInput.length <= 4
+      ? probationOfficerPhoneInput
+      : new AsYouType('US').input(probationOfficerPhoneInput);
+    formDataWithPhoneNumbersFormatted = setIn(
+      formDataWithPhoneNumbersFormatted,
+      probationOfficerPhoneKey,
+      probationOfficerPhone
+    );
+  }
 
   return formDataWithPhoneNumbersFormatted;
 };
 
 const validateParticipantPhoneNumbers = (formData :Object, errors :Object) => {
-  const pageSection1 = getPageSectionKey(1, 2);
+  const participantContactsPageSectionKey = getPageSectionKey(1, 2);
   const homePhoneKey = getEntityAddressKey(0, CONTACT_INFO, PHONE_NUMBER);
   const cellPhoneKey = getEntityAddressKey(1, CONTACT_INFO, PHONE_NUMBER);
 
+  const otherContactsPageSectionKeys = [getPageSectionKey(1, 5), getPageSectionKey(1, 6)];
+  const attorneyPhonePath :string[] = otherContactsPageSectionKeys
+    .concat([getEntityAddressKey(-2, CONTACT_INFO, PHONE_NUMBER)]);
+  const probationOfficerPath :string[] = otherContactsPageSectionKeys
+    .concat([getEntityAddressKey(-4, CONTACT_INFO, PHONE_NUMBER)]);
+
   const errorMessage :string = 'Phone number is invalid';
 
-  const homePhone :string = getIn(formData, [pageSection1, homePhoneKey]);
+  const homePhone :string = getIn(formData, [participantContactsPageSectionKey, homePhoneKey]);
   if (homePhone) {
     const parsedHomePhone = parsePhoneNumberFromString(homePhone, 'US');
     if (!parsedHomePhone.isPossible()) {
-      errors[pageSection1][homePhoneKey].addError(errorMessage);
+      errors[participantContactsPageSectionKey][homePhoneKey].addError(errorMessage);
     }
   }
 
-  const cellPhone :string = getIn(formData, [pageSection1, cellPhoneKey]);
+  const cellPhone :string = getIn(formData, [participantContactsPageSectionKey, cellPhoneKey]);
   if (cellPhone) {
     const parsedCellPhone = parsePhoneNumberFromString(cellPhone, 'US');
     if (!parsedCellPhone.isPossible()) {
-      errors[pageSection1][cellPhoneKey].addError(errorMessage);
+      errors[participantContactsPageSectionKey][cellPhoneKey].addError(errorMessage);
+    }
+  }
+
+  const attorneyPhone :string = getIn(formData, attorneyPhonePath);
+  if (attorneyPhone) {
+    const parsedAttorneyPhone = parsePhoneNumberFromString(attorneyPhone, 'US');
+    if (!parsedAttorneyPhone.isPossible()) {
+      errors[attorneyPhonePath[0]][attorneyPhonePath[1]][attorneyPhonePath[2]].addError(errorMessage);
+    }
+  }
+
+  const probationOfficerPhone :string = getIn(formData, probationOfficerPath);
+  if (probationOfficerPhone) {
+    const parsedProbationOfficerPhone = parsePhoneNumberFromString(probationOfficerPhone, 'US');
+    if (!parsedProbationOfficerPhone.isPossible()) {
+      errors[probationOfficerPath[0]][probationOfficerPath[1]][probationOfficerPath[2]].addError(errorMessage);
     }
   }
 
