1f67ef8551843789d8fecd4cf52376df84417833
Giulia Campana <giuliaecampana@gmail.com>
2020-08-25 17:02:05 -0700

    add task and meeting fields to the schema

--- src/containers/casenotes/schemas/CaseNotesSchemas.js
+++ src/containers/casenotes/schemas/CaseNotesSchemas.js
@@ -1,14 +1,20 @@
 // @flow
 import { DataProcessingUtils } from 'lattice-fabricate';
+import { DateTime } from 'luxon';
 
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { FOLLOW_UPS_STATUSES } from '../../profile/tasks/FollowUpsConstants';
 
 const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
-const { MEETINGS, REENTRY_STAFF } = APP_TYPE_FQNS;
+const { FOLLOW_UPS, MEETINGS, REENTRY_STAFF } = APP_TYPE_FQNS;
 const {
+  DATETIME_COMPLETED,
+  DATETIME_END,
   ENTITY_KEY_ID,
   FUTURE_PLANS,
   GENERAL_NOTES,
+  GENERAL_STATUS,
+  STATUS,
   VISIT_REASON,
 } = PROPERTY_TYPE_FQNS;
 
@@ -32,6 +38,16 @@ const schema :Object = {
           type: 'string',
           title: 'Plans for Next Visit',
         },
+        [getEntityAddressKey(0, MEETINGS, DATETIME_END)]: {
+          type: 'string',
+          title: 'Completed datetime',
+          default: DateTime.local().toISO(),
+        },
+        [getEntityAddressKey(0, MEETINGS, GENERAL_STATUS)]: {
+          type: 'string',
+          title: 'Meeting status',
+          default: FOLLOW_UPS_STATUSES.DONE,
+        },
       },
     },
     [getPageSectionKey(1, 2)]: {
@@ -46,6 +62,22 @@ const schema :Object = {
         },
       },
       required: [getEntityAddressKey(0, REENTRY_STAFF, ENTITY_KEY_ID)]
+    },
+    [getPageSectionKey(1, 3)]: {
+      type: 'object',
+      title: '',
+      properties: {
+        [getEntityAddressKey(0, FOLLOW_UPS, DATETIME_COMPLETED)]: {
+          type: 'string',
+          title: 'Completed datetime',
+          default: DateTime.local().toISO(),
+        },
+        [getEntityAddressKey(0, FOLLOW_UPS, STATUS)]: {
+          type: 'string',
+          title: 'Task status',
+          default: FOLLOW_UPS_STATUSES.DONE,
+        },
+      }
     }
   }
 };
@@ -65,12 +97,26 @@ const uiSchema :Object = {
       classNames: 'column-span-12',
       'ui:widget': 'TextareaWidget',
     },
+    [getEntityAddressKey(0, MEETINGS, DATETIME_END)]: {
+      'ui:widget': 'hidden',
+    },
+    [getEntityAddressKey(0, MEETINGS, GENERAL_STATUS)]: {
+      'ui:widget': 'hidden',
+    },
   },
   [getPageSectionKey(1, 2)]: {
     classNames: 'column-span-12 grid-container',
     [getEntityAddressKey(0, REENTRY_STAFF, ENTITY_KEY_ID)]: {
       classNames: 'column-span-6',
     }
+  },
+  [getPageSectionKey(1, 3)]: {
+    [getEntityAddressKey(0, FOLLOW_UPS, DATETIME_COMPLETED)]: {
+      'ui:widget': 'hidden',
+    },
+    [getEntityAddressKey(0, FOLLOW_UPS, STATUS)]: {
+      'ui:widget': 'hidden',
+    },
   }
 };
 
