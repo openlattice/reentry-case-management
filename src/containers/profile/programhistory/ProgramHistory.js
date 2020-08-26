e5b8a21e766ffc7d1625fd976d31cb2ac41e96fa
Giulia Campana <giuliaecampana@gmail.com>
2020-08-26 16:21:24 -0700

    add facility name to profile

--- src/containers/profile/programhistory/ProgramHistory.js
+++ src/containers/profile/programhistory/ProgramHistory.js
@@ -16,7 +16,7 @@ import { schema, uiSchema } from './schemas/EditEnrollmentDateSchemas';
 
 import EditButton from '../../../components/buttons/EditButton';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { getEKID } from '../../../utils/DataUtils';
+import { getEKID, getEntityProperties } from '../../../utils/DataUtils';
 import { sortEntitiesByDateProperty } from '../../../utils/Utils';
 import { EMPTY_FIELD } from '../../../utils/constants/GeneralConstants';
 import {
@@ -32,12 +32,14 @@ import { getMostRecentReleaseDate, getReentryEnrollmentDate } from '../utils/Pro
 const { NEUTRAL } = Colors;
 const {
   ENROLLMENT_STATUS,
+  MANUAL_JAILS_PRISONS,
   MANUAL_JAIL_STAYS,
   NEEDS_ASSESSMENT,
   REFERRAL_REQUEST,
 } = APP_TYPE_FQNS;
 const {
   EFFECTIVE_DATE,
+  NAME,
   SOURCE,
 } = PROPERTY_TYPE_FQNS;
 
@@ -74,11 +76,15 @@ const ProgramHistory = ({
   const enrollmentDate :string = getReentryEnrollmentDate(participantNeighbors);
   const referralSource :string = `Referred from: ${participantNeighbors
     .getIn([REFERRAL_REQUEST, 0, SOURCE, 0], EMPTY_FIELD)}`;
+  const releaseDate = getMostRecentReleaseDate(participantNeighbors.get(MANUAL_JAIL_STAYS, List()));
+  const facilityList = participantNeighbors.get(MANUAL_JAILS_PRISONS, List());
+  const facility :Map = facilityList.get(0, Map());
+  const { [NAME]: facilityReleasedFrom } = getEntityProperties(facility, [NAME]);
+  const releaseText
+    :string = `Released from: ${facilityReleasedFrom || 'Unknown'}${releaseDate ? ` on ${releaseDate}` : ''}`;
 
   let enrollmentEvents :List = participantNeighbors.get(ENROLLMENT_STATUS, List());
   enrollmentEvents = sortEntitiesByDateProperty(enrollmentEvents, [EFFECTIVE_DATE]).reverse();
-  const releaseDate = getMostRecentReleaseDate(participantNeighbors.get(MANUAL_JAIL_STAYS, List()));
-  const releaseText :string = `Released: ${releaseDate}`;
   const needsAssessment :Map = participantNeighbors.getIn([NEEDS_ASSESSMENT, 0], Map());
   return (
     <EventsCard>
@@ -88,7 +94,7 @@ const ProgramHistory = ({
       </CardHeaderWithButtons>
       <GrayBar padding="15px 30px" vertical={false}>
         <div>{ referralSource }</div>
-        { releaseDate && (<div>{ releaseText }</div>) }
+        { (releaseDate || facilityReleasedFrom) && (<div>{ releaseText }</div>) }
       </GrayBar>
       {
         enrollmentEvents.map((enrollmentStatus :Map) => (
