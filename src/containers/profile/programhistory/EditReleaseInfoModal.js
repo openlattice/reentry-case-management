c4a1aea16a1bcf61e485fb12df60fee53f1ae61e
Giulia Campana <giuliaecampana@gmail.com>
2020-08-28 11:37:46 -0700

    hydrate schema with incarceration facilities

--- src/containers/profile/programhistory/EditReleaseInfoModal.js
+++ src/containers/profile/programhistory/EditReleaseInfoModal.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { useCallback, useEffect, useState } from 'react';
+
 import styled from 'styled-components';
 import {
   List,
@@ -7,25 +8,27 @@ import {
   getIn,
   setIn,
 } from 'immutable';
-import { DateTime } from 'luxon';
-import { Modal, ModalFooter } from 'lattice-ui-kit';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Modal, ModalFooter } from 'lattice-ui-kit';
+import { DateTime } from 'luxon';
 import { useDispatch, useSelector } from 'react-redux';
 
-import ModalHeader from '../../../components/modal/ModalHeader';
-import { schema, uiSchema } from './schemas/EditReleaseInfoSchemas';
-import { requestIsPending, requestIsSuccess } from '../../../utils/RequestStateUtils';
-import { getEKID } from '../../../utils/DataUtils';
-import { getReleaseDateAndEKIDForForm } from '../utils/ProfileUtils';
 import { EDIT_RELEASE_INFO, editReleaseInfo } from './ProgramHistoryActions';
-import { clearEditRequestState } from '../needs/NeedsActions';
+import { schema, uiSchema } from './schemas/EditReleaseInfoSchemas';
+
+import ModalHeader from '../../../components/modal/ModalHeader';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
+import { getEKID } from '../../../utils/DataUtils';
+import { requestIsPending, requestIsSuccess } from '../../../utils/RequestStateUtils';
 import {
   APP,
   EDM,
   PROFILE,
   SHARED
 } from '../../../utils/constants/ReduxStateConstants';
+import { clearEditRequestState } from '../needs/NeedsActions';
+import { getDataForAssociationUpdate, hydrateSchema } from '../utils/EditReleaseInfoUtils';
+import { getReleaseDateAndEKIDForForm } from '../utils/ProfileUtils';
 
 const {
   findEntityAddressKeyFromMap,
@@ -34,8 +37,8 @@ const {
   processEntityDataForPartialReplace,
   replaceEntityAddressKeys,
 } = DataProcessingUtils;
-const { MANUAL_JAIL_STAYS, REFERRAL_REQUEST } = APP_TYPE_FQNS;
-const { PROJECTED_RELEASE_DATETIME, SOURCE } = PROPERTY_TYPE_FQNS;
+const { MANUAL_JAILS_PRISONS, MANUAL_JAIL_STAYS, REFERRAL_REQUEST } = APP_TYPE_FQNS;
+const { ENTITY_KEY_ID, PROJECTED_RELEASE_DATETIME, SOURCE } = PROPERTY_TYPE_FQNS;
 const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
 const { TYPE_IDS_BY_FQN, PROPERTY_TYPES } = EDM;
 const { ACTIONS, REQUEST_STATE } = SHARED;
@@ -46,20 +49,30 @@ const FixedWidthModal = styled.div`
 `;
 
 type Props = {
+  incarcerationFacilities :List;
   isVisible :boolean;
   onClose :() => void;
   participantNeighbors :Map;
 };
 
-const EditReleaseInfoModal = ({ isVisible, onClose, participantNeighbors } :Props) => {
+const EditReleaseInfoModal = ({
+  incarcerationFacilities,
+  isVisible,
+  onClose,
+  participantNeighbors
+} :Props) => {
+
+  const schemaWithFacilities = hydrateSchema(schema, incarcerationFacilities);
 
   const referralSource = participantNeighbors.getIn([REFERRAL_REQUEST, 0, SOURCE, 0]);
   const { jailStayEKID, releaseDate } = getReleaseDateAndEKIDForForm(
     participantNeighbors.get(MANUAL_JAIL_STAYS,
       List())
   );
+  const facilityEKID = getEKID(participantNeighbors.getIn([MANUAL_JAILS_PRISONS, 0]));
   const originalFormData = {
     [getPageSectionKey(1, 1)]: {
+      [getEntityAddressKey(0, MANUAL_JAILS_PRISONS, ENTITY_KEY_ID)]: facilityEKID,
       [getEntityAddressKey(0, MANUAL_JAIL_STAYS, PROJECTED_RELEASE_DATETIME)]: releaseDate,
       [getEntityAddressKey(0, REFERRAL_REQUEST, SOURCE)]: referralSource,
     }
@@ -123,9 +136,10 @@ const EditReleaseInfoModal = ({ isVisible, onClose, participantNeighbors } :Prop
       const releaseDateTime = DateTime.fromSQL(`${formReleaseDate} ${currentTime}`).toISO();
       updatedFormData = setIn(updatedFormData, projectedReleaseDatetimePath, releaseDateTime);
     }
+    const { editedFormData, newFacilityEKID } = getDataForAssociationUpdate(updatedFormData, facilityEKID);
 
     const draftWithKeys :Object = replaceEntityAddressKeys(
-      updatedFormData,
+      editedFormData,
       findEntityAddressKeyFromMap(entityIndexToIdMap)
     );
     const entityData = processEntityDataForPartialReplace(
@@ -134,8 +148,8 @@ const EditReleaseInfoModal = ({ isVisible, onClose, participantNeighbors } :Prop
       entitySetIds,
       propertyTypeIds,
     );
-    if (Object.values(entityData).length) {
-      dispatch(editReleaseInfo({ entityData, personEKID }));
+    if (Object.values(entityData).length || newFacilityEKID.length) {
+      dispatch(editReleaseInfo({ entityData, newFacilityEKID, personEKID }));
     }
     else {
       onClose();
@@ -175,7 +189,7 @@ const EditReleaseInfoModal = ({ isVisible, onClose, participantNeighbors } :Prop
             noPadding
             onChange={onChange}
             onSubmit={onSubmit}
-            schema={schema}
+            schema={schemaWithFacilities}
             uiSchema={uiSchema} />
       </FixedWidthModal>
     </Modal>
