ea9e91000d147ece6c064da7fae690aedcb92b7a
Giulia Campana <giuliaecampana@gmail.com>
2020-09-01 19:38:58 -0700

    update editFacilityForm to handle changing or creating first association between jail stay and facility

--- src/containers/profile/programhistory/EditFacilityForm.js
+++ src/containers/profile/programhistory/EditFacilityForm.js
@@ -1,78 +1,35 @@
 // @flow
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import styled from 'styled-components';
-import {
-  List,
-  Map,
-  getIn,
-  setIn,
-} from 'immutable';
+import { List, Map } from 'immutable';
 import { DataProcessingUtils, Form } from 'lattice-fabricate';
-import {
-  Breadcrumbs,
-  Card,
-  CardSegment,
-  CardStack,
-} from 'lattice-ui-kit';
-import {
-  DataUtils,
-  LangUtils,
-  RoutingUtils,
-  useGoToRoute,
-  useRequestState,
-} from 'lattice-utils';
-import { DateTime } from 'luxon';
+import { Card, CardSegment, Spinner } from 'lattice-ui-kit';
+import { DataUtils, LangUtils, useRequestState } from 'lattice-utils';
 import { useDispatch, useSelector } from 'react-redux';
-import type { Match } from 'react-router';
 
-import { EDIT_RELEASE_INFO, editReleaseInfo } from './ProgramHistoryActions';
-import {
-  facilitySchema,
-  facilityUiSchema,
-  referredFromSchema,
-  referredFromUiSchema,
-  releaseDateSchema,
-  releaseDateUiSchema,
-} from './schemas/EditReleaseInfoSchemas';
+import { EDIT_FACILITY_RELEASED_FROM, editFacilityReleasedFrom } from './ProgramHistoryActions';
+import { facilitySchema, facilityUiSchema } from './schemas/EditReleaseInfoSchemas';
 
-import * as Routes from '../../../core/router/Routes';
 import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { getEKID } from '../../../utils/DataUtils';
-import { getPersonFullName } from '../../../utils/PeopleUtils';
-import { requestIsPending, requestIsSuccess } from '../../../utils/RequestStateUtils';
-import { DST } from '../../../utils/constants/GeneralConstants';
+import { requestIsPending } from '../../../utils/RequestStateUtils';
 import {
   APP,
-  EDM,
   INTAKE,
   PROFILE,
   SHARED
 } from '../../../utils/constants/ReduxStateConstants';
-import { getIncarcerationFacilities } from '../../intake/IntakeActions';
-import { getParticipant, getParticipantNeighbors } from '../ProfileActions';
-import { clearEditRequestState } from '../needs/NeedsActions';
-import { Header, NameHeader } from '../styled/GeneralProfileStyles';
-import { getDataForAssociationUpdate, hydrateSchema } from '../utils/EditReleaseInfoUtils';
+import { hydrateSchema } from '../utils/EditReleaseInfoUtils';
 import { getReleaseDateAndEKIDForForm } from '../utils/ProfileUtils';
 
 const { getEntityKeyId } = DataUtils;
 const { isDefined } = LangUtils;
-const {
-  findEntityAddressKeyFromMap,
-  getEntityAddressKey,
-  getPageSectionKey,
-  processEntityDataForPartialReplace,
-  replaceEntityAddressKeys,
-} = DataProcessingUtils;
-const { getParamFromMatch } = RoutingUtils;
-const { MANUAL_JAILS_PRISONS, MANUAL_JAIL_STAYS, REFERRAL_REQUEST } = APP_TYPE_FQNS;
-const { ENTITY_KEY_ID, PROJECTED_RELEASE_DATETIME, SOURCE } = PROPERTY_TYPE_FQNS;
+const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
+const { MANUAL_JAILS_PRISONS, MANUAL_JAIL_STAYS, MANUAL_LOCATED_AT } = APP_TYPE_FQNS;
+const { ENTITY_KEY_ID } = PROPERTY_TYPE_FQNS;
 const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
-const { TYPE_IDS_BY_FQN, PROPERTY_TYPES } = EDM;
-const { ACTIONS, REQUEST_STATE } = SHARED;
+const { ACTIONS } = SHARED;
 const { INCARCERATION_FACILITIES } = INTAKE;
-const { PARTICIPANT, PARTICIPANT_NEIGHBORS } = PROFILE;
+const { PARTICIPANT_NEIGHBORS } = PROFILE;
 
 const EditFacilityForm = () => {
 
@@ -82,11 +39,6 @@ const EditFacilityForm = () => {
     ENTITY_SET_IDS_BY_ORG_ID,
     selectedOrgId
   ], Map()));
-  const propertyTypeIds :Map = useSelector((store :Map) => store.getIn([
-    EDM.EDM,
-    TYPE_IDS_BY_FQN,
-    PROPERTY_TYPES
-  ], Map()));
 
   const dispatch = useDispatch();
 
@@ -95,7 +47,8 @@ const EditFacilityForm = () => {
     PARTICIPANT_NEIGHBORS
   ], Map()));
 
-  const facilityEKID = getEKID(participantNeighbors.getIn([MANUAL_JAILS_PRISONS, 0]));
+  const facilityEKID = getEntityKeyId(participantNeighbors.getIn([MANUAL_JAILS_PRISONS, 0]));
+  const { jailStayEKID } = getReleaseDateAndEKIDForForm(participantNeighbors.get(MANUAL_JAIL_STAYS, List()));
   const [formData, updateFormData] = useState({});
 
   useEffect(() => {
@@ -111,54 +64,30 @@ const EditFacilityForm = () => {
     updateFormData(newFormData);
   };
 
-  const editReleaseInfoReqState = useSelector((store :Map) => store.getIn([
-    PROFILE.PROFILE,
-    ACTIONS,
-    EDIT_RELEASE_INFO,
-    REQUEST_STATE
-  ]));
-
-  useEffect(() => {
-    if (requestIsSuccess(editReleaseInfoReqState)) {
-      dispatch(clearEditRequestState());
-    }
-  }, [dispatch, editReleaseInfoReqState]);
-
-  const participant :Map = useSelector((store :Map) => store.getIn([
-    PROFILE.PROFILE,
-    PARTICIPANT
-  ], Map()));
-  const personEKID = getEntityKeyId(participant);
-
-  const onSubmit = () => {
-    let updatedFormData = formData;
-    const projectedReleaseDatetimePath = [
-      getPageSectionKey(1, 1),
-      getEntityAddressKey(0, MANUAL_JAIL_STAYS, PROJECTED_RELEASE_DATETIME)
+  const onSubmit = ({ formData: submittedFormData }) => {
+    const facilityESID = entitySetIds.get(MANUAL_JAILS_PRISONS);
+    const locatedAtESID = entitySetIds.get(MANUAL_LOCATED_AT);
+    const jailStaysESID = entitySetIds.get(MANUAL_JAIL_STAYS);
+    const newFacilityEKID = submittedFormData[getPageSectionKey(1, 1)][
+      getEntityAddressKey(0, MANUAL_JAILS_PRISONS, ENTITY_KEY_ID)
     ];
-    if (getIn(formData, projectedReleaseDatetimePath)) {
-      const formReleaseDate = getIn(formData, projectedReleaseDatetimePath);
-      const currentTime = DateTime.local().toLocaleString(DateTime.TIME_24_SIMPLE);
-      const releaseDateTime = DateTime.fromSQL(`${formReleaseDate} ${currentTime}`).toISO();
-      updatedFormData = setIn(updatedFormData, projectedReleaseDatetimePath, releaseDateTime);
-    }
-    const { editedFormData, newFacilityEKID } = getDataForAssociationUpdate(updatedFormData, facilityEKID);
-
-    const draftWithKeys :Object = replaceEntityAddressKeys(
-      editedFormData,
-      findEntityAddressKeyFromMap(entityIndexToIdMap)
-    );
-    const entityData = processEntityDataForPartialReplace(
-      draftWithKeys,
-      replaceEntityAddressKeys(originalFormData, findEntityAddressKeyFromMap(entityIndexToIdMap)),
-      entitySetIds,
-      propertyTypeIds,
-    );
-    if (Object.values(entityData).length || newFacilityEKID.length) {
-      dispatch(editReleaseInfo({ entityData, newFacilityEKID, personEKID }));
-    }
-    else {
-      // onClose();
+    if (facilityEKID !== newFacilityEKID) {
+      const associations = {
+        [locatedAtESID]: [
+          {
+            data: {},
+            src: {
+              entitySetId: jailStaysESID,
+              entityKeyId: jailStayEKID
+            },
+            dst: {
+              entitySetId: facilityESID,
+              entityKeyId: newFacilityEKID
+            }
+          }
+        ]
+      };
+      dispatch(editFacilityReleasedFrom({ associations, jailStayEKID, newFacilityEKID }));
     }
   };
 
@@ -169,13 +98,37 @@ const EditFacilityForm = () => {
 
   const facilitySchemaWithFacilities = hydrateSchema(facilitySchema, incarcerationFacilities);
 
+  const formContext = {
+    editAction: onSubmit,
+    entityIndexToIdMap: Map(),
+    entitySetIds,
+    propertyTypeIds: Map(),
+  };
+
+  const editRequestState = useRequestState([
+    PROFILE.PROFILE,
+    ACTIONS,
+    EDIT_FACILITY_RELEASED_FROM,
+  ]);
+
+  if (requestIsPending(editRequestState)) {
+    return (
+      <Card>
+        <CardSegment vertical>
+          <Spinner size="2x" />
+        </CardSegment>
+      </Card>
+    );
+  }
+
   return (
     <Card>
-      <CardSegment>
+      <CardSegment padding={isDefined(facilityEKID) ? '30px' : '0'}>
         <Form
-            disabled={isDefined(facilityEKID)}
+            disabled={isDefined(facilityEKID) || !isDefined(jailStayEKID)}
+            formContext={formContext}
             formData={formData}
-            noPadding
+            noPadding={isDefined(facilityEKID)}
             onChange={onChange}
             onSubmit={onSubmit}
             schema={facilitySchemaWithFacilities}
