ef67ef9d4f656ba223b8a94c13b92ee72906951f
Giulia Campana <giuliaecampana@gmail.com>
2020-09-01 20:16:13 -0700

    update parent form

--- src/containers/profile/programhistory/EditReleaseInfoForm.js
+++ src/containers/profile/programhistory/EditReleaseInfoForm.js
@@ -1,47 +1,28 @@
 // @flow
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import styled from 'styled-components';
-import {
-  List,
-  Map,
-  getIn,
-  setIn,
-} from 'immutable';
-import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Map } from 'immutable';
 import {
   Breadcrumbs,
   CardSegment,
   CardStack,
   Spinner,
 } from 'lattice-ui-kit';
-import {
-  DataUtils,
-  LangUtils,
-  ReduxUtils,
-  RoutingUtils,
-  useGoToRoute,
-  useRequestState,
-} from 'lattice-utils';
-import { DateTime } from 'luxon';
+import { ReduxUtils, RoutingUtils, useRequestState } from 'lattice-utils';
 import { useDispatch, useSelector } from 'react-redux';
-import { RequestStates } from 'redux-reqseq';
 import type { Match } from 'react-router';
 
 import EditFacilityForm from './EditFacilityForm';
 import EditReferredFromForm from './EditReferredFromForm';
 import EditReleaseDateForm from './EditReleaseDateForm';
-import { EDIT_RELEASE_INFO, editReleaseInfo } from './ProgramHistoryActions';
 
 import * as Routes from '../../../core/router/Routes';
-import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { getEKID } from '../../../utils/DataUtils';
+import { APP_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 import { getPersonFullName } from '../../../utils/PeopleUtils';
-import { requestIsPending, requestIsSuccess } from '../../../utils/RequestStateUtils';
+import { requestIsPending } from '../../../utils/RequestStateUtils';
 import { DST } from '../../../utils/constants/GeneralConstants';
 import {
   APP,
-  EDM,
   INTAKE,
   PROFILE,
   SHARED
@@ -53,18 +34,14 @@ import {
   getParticipant,
   getParticipantNeighbors,
 } from '../ProfileActions';
-import { clearEditRequestState } from '../needs/NeedsActions';
 import { Header, NameHeader } from '../styled/GeneralProfileStyles';
 
 const { reduceRequestStates } = ReduxUtils;
 const { getParamFromMatch } = RoutingUtils;
 const { MANUAL_JAILS_PRISONS, MANUAL_JAIL_STAYS, REFERRAL_REQUEST } = APP_TYPE_FQNS;
-const { ENTITY_KEY_ID, PROJECTED_RELEASE_DATETIME, SOURCE } = PROPERTY_TYPE_FQNS;
 const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
-const { TYPE_IDS_BY_FQN, PROPERTY_TYPES } = EDM;
-const { ACTIONS, REQUEST_STATE } = SHARED;
-const { INCARCERATION_FACILITIES } = INTAKE;
-const { PARTICIPANT, PARTICIPANT_NEIGHBORS } = PROFILE;
+const { ACTIONS } = SHARED;
+const { PARTICIPANT } = PROFILE;
 
 type Props = {
   match :Match;
@@ -99,19 +76,6 @@ const EditReleaseInfoForm = ({ match } :Props) => {
     }));
     dispatch(getParticipant({ participantEKID }));
   }, [dispatch, manualJailsPrisonsESID, manualjailStaysESID, participantEKID, referralRequestESID]);
-
-  // const editReleaseInfoReqState = useSelector((store :Map) => store.getIn([
-  //   PROFILE.PROFILE,
-  //   ACTIONS,
-  //   EDIT_RELEASE_INFO,
-  //   REQUEST_STATE
-  // ]));
-  //
-  // useEffect(() => {
-  //   if (requestIsSuccess(editReleaseInfoReqState)) {
-  //     dispatch(clearEditRequestState());
-  //   }
-  // }, [dispatch, editReleaseInfoReqState]);
 
   const participant :Map = useSelector((store :Map) => store.getIn([
     PROFILE.PROFILE,
