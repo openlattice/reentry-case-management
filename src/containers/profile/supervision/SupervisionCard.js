4e7a0bc06ec86c2dc8bbd384e2d48c3233d1745f
Giulia Campana <giuliaecampana@gmail.com>
2020-09-09 17:33:34 -0700

    use util instead of formatting data in actual Card

--- src/containers/profile/supervision/SupervisionCard.js
+++ src/containers/profile/supervision/SupervisionCard.js
@@ -1,30 +1,28 @@
 // @flow
 import React from 'react';
 
-import { List, Map } from 'immutable';
+import { Map, OrderedMap } from 'immutable';
 import {
   Card,
   CardHeader,
   CardSegment,
   DataGrid,
 } from 'lattice-ui-kit';
-import { DateTime } from 'luxon';
 
-import { APP_TYPE_FQNS, PROPERTY_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
-import { getEntityProperties } from '../../../utils/DataUtils';
-import { getPersonFullName } from '../../../utils/PeopleUtils';
 import { EMPTY_FIELD } from '../../../utils/constants/GeneralConstants';
 import { SmallCardHeaderTitle } from '../styled/GeneralProfileStyles';
+import { formatGridData } from '../utils/SupervisionUtils';
 
-const { ATTORNEYS, OFFICERS, PROBATION_PAROLE } = APP_TYPE_FQNS;
-const { LEVEL, RECOGNIZED_END_DATETIME, TYPE } = PROPERTY_TYPE_FQNS;
-
-const labelMap = Map({
+const labelMap = OrderedMap({
   supervisionType: 'Supervision type',
   level: 'Level',
   endDate: 'End date',
   officerName: 'Officer name',
+  officerPhone: 'Officer phone',
+  officerEmail: 'Officer email',
   attorneyName: 'Attorney name',
+  attorneyPhone: 'Attorney phone',
+  attorneyEmail: 'Attorney email',
 });
 
 type Props = {
@@ -33,22 +31,7 @@ type Props = {
 };
 
 const SupervisionCard = ({ participantNeighbors, supervisionNeighbors } :Props) => {
-  const supervisionList :List = participantNeighbors.get(PROBATION_PAROLE, List());
-  const {
-    [LEVEL]: level,
-    [RECOGNIZED_END_DATETIME]: endDateTime,
-    [TYPE]: supervisionType
-  } = getEntityProperties(supervisionList.get(0), [LEVEL, RECOGNIZED_END_DATETIME, TYPE]);
-
-  const attorney :Map = supervisionNeighbors.get(ATTORNEYS, Map());
-  const officer :Map = supervisionNeighbors.get(OFFICERS, Map());
-  const gridData = Map({
-    supervisionType,
-    level,
-    endDate: DateTime.fromISO(endDateTime).toLocaleString(DateTime.DATE_SHORT),
-    officerName: getPersonFullName(officer),
-    attorneyName: getPersonFullName(attorney),
-  });
+  const gridData = formatGridData(participantNeighbors, supervisionNeighbors);
   return (
     <Card>
       <CardHeader padding="30px" vertical={false}>
