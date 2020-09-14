09c9147c299a849264037fb2f82815c04c65876d
Giulia Campana <giuliaecampana@gmail.com>
2020-09-14 15:22:48 -0400

    make EditSupervisionForm parent container for sub-forms

--- src/containers/profile/supervision/EditSupervisionForm.js
+++ src/containers/profile/supervision/EditSupervisionForm.js
@@ -1,19 +1,18 @@
 // @flow
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { List, Map } from 'immutable';
-import { DataProcessingUtils, Form } from 'lattice-fabricate';
+import { Map } from 'immutable';
 import {
   Breadcrumbs,
-  Card,
   CardSegment,
+  CardStack,
   Spinner,
 } from 'lattice-ui-kit';
 import { ReduxUtils, RoutingUtils, useRequestState } from 'lattice-utils';
 import { useDispatch, useSelector } from 'react-redux';
 import type { Match } from 'react-router';
 
-import { schema, uiSchema } from './schemas/EditSupervisionSchemas';
+import EditProbationParoleForm from './EditProbationParoleForm';
 
 import { APP_TYPE_FQNS } from '../../../core/edm/constants/FullyQualifiedNames';
 import {
@@ -37,10 +36,8 @@ import {
   getParticipantNeighbors,
 } from '../ProfileActions';
 import { Header, NameHeader } from '../styled/GeneralProfileStyles';
-import { getOriginalFormData } from '../utils/SupervisionUtils';
 
 const { getParamFromMatch } = RoutingUtils;
-const { getEntityAddressKey, getPageSectionKey } = DataProcessingUtils;
 const { reduceRequestStates } = ReduxUtils;
 const { EMPLOYEE, EMPLOYMENT, PROBATION_PAROLE } = APP_TYPE_FQNS;
 const { ENTITY_SET_IDS_BY_ORG_ID, SELECTED_ORG_ID } = APP;
@@ -93,25 +90,6 @@ const EditSupervisionForm = ({ match } :Props) => {
   ], Map()));
   const personName = getPersonFullName(participant);
 
-  const [formData, updateFormData] = useState({});
-
-  useEffect(() => {
-    const originalFormData = getOriginalFormData(participantNeighbors, supervisionNeighbors);
-    updateFormData(originalFormData);
-  }, [participantNeighbors, supervisionNeighbors]);
-
-  const onChange = ({ formData: newFormData } :Object) => {
-    updateFormData(newFormData);
-  };
-
-  const entityIndexToIdMap :Map = Map({
-    [PROBATION_PAROLE]: List([jailStayEKID]),
-  });
-
-  const onSubmit = ({ formData: submittedFormData }) => {
-    console.log('submittedFormData ', submittedFormData);
-  };
-
   const getParticipantRequestState = useRequestState([
     PROFILE.PROFILE,
     ACTIONS,
@@ -141,20 +119,15 @@ const EditSupervisionForm = ({ match } :Props) => {
                     { personName }
                   </NameHeader>
                   <NameHeader to={EDIT_SUPERVISION.replace(PARTICIPANT_ID, participantEKID)}>
-                    Edit Release Information
+                    Edit Supervision
                   </NameHeader>
                 </Breadcrumbs>
               </CardSegment>
-              <Card>
-                <CardSegment padding="0">
-                  <Form
-                      formData={formData}
-                      onChange={onChange}
-                      onSubmit={onSubmit}
-                      schema={schema}
-                      uiSchema={uiSchema} />
-                </CardSegment>
-              </Card>
+              <CardStack>
+                <EditProbationParoleForm
+                    participantEKID={participantEKID}
+                    participantNeighbors={participantNeighbors} />
+              </CardStack>
             </>
           )
       }
