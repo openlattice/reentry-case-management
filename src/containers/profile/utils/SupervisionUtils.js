a8f8c936614c31ee5651138687acdd039e172f71
Giulia Campana <giuliaecampana@gmail.com>
2020-09-15 16:58:20 -0400

    remove unused util

--- src/containers/profile/utils/SupervisionUtils.js
+++ src/containers/profile/utils/SupervisionUtils.js
@@ -88,91 +88,6 @@ const formatGridData = (participantNeighbors :Map, supervisionNeighbors :Map) :M
 
 // SupervisionForm
 
-const getOriginalFormData = (participantNeighbors :Map, supervisionNeighbors :Map) :Object => {
-
-  const originalFormData = {};
-
-  if (participantNeighbors.isEmpty()) return originalFormData;
-  const supervision = participantNeighbors.get(PROBATION_PAROLE, List());
-  const {
-    [LEVEL]: level,
-    [RECOGNIZED_END_DATETIME]: endDateTimeISO,
-    [TYPE]: type
-  } = getEntityProperties(supervision.get(0) || Map(), [LEVEL, RECOGNIZED_END_DATETIME, TYPE], undefined);
-  const endDateTime = DateTime.fromISO(endDateTimeISO);
-
-  originalFormData[getPageSectionKey(1, 1)] = {
-    [getEntityAddressKey(0, PROBATION_PAROLE, TYPE)]: type,
-    [getEntityAddressKey(0, PROBATION_PAROLE, RECOGNIZED_END_DATETIME)]: endDateTime.isValid
-      ? endDateTime.toISODate()
-      : undefined,
-    [getEntityAddressKey(0, PROBATION_PAROLE, LEVEL)]: level,
-  };
-
-  const officer :Map = supervisionNeighbors.get(OFFICERS, Map());
-  const attorney :Map = supervisionNeighbors.get(ATTORNEYS, Map());
-
-  const { [FIRST_NAME]: officerFirstName, [LAST_NAME]: officerLastName } = getEntityProperties(
-    officer,
-    [FIRST_NAME, LAST_NAME],
-    undefined
-  );
-
-  originalFormData[getPageSectionKey(1, 2)] = {
-    [getEntityAddressKey(0, OFFICERS, FIRST_NAME)]: officerFirstName,
-    [getEntityAddressKey(0, OFFICERS, LAST_NAME)]: officerLastName,
-  };
-
-  const { [FIRST_NAME]: attorneyFirstName, [LAST_NAME]: attorneyLastName } = getEntityProperties(
-    attorney,
-    [FIRST_NAME, LAST_NAME],
-    undefined
-  );
-
-  originalFormData[getPageSectionKey(1, 4)] = {
-    [getEntityAddressKey(0, ATTORNEYS, FIRST_NAME)]: attorneyFirstName,
-    [getEntityAddressKey(0, ATTORNEYS, LAST_NAME)]: attorneyLastName,
-  };
-
-  const contactInfo :Map = supervisionNeighbors.get(CONTACT_INFO, Map());
-  const officerContactInfo :List = contactInfo.get(OFFICERS, List());
-  const attorneyContactInfo :List = contactInfo.get(ATTORNEYS, List());
-
-  let officerPhone;
-  let officerEmail;
-  officerContactInfo.forEach((contact :Map) => {
-    if (contact.has(PHONE_NUMBER)) {
-      officerPhone = contact.getIn([PHONE_NUMBER, 0]);
-    }
-    if (contact.has(EMAIL)) {
-      officerEmail = contact.getIn([EMAIL, 0]);
-    }
-  });
-
-  originalFormData[getPageSectionKey(1, 3)] = {
-    [getEntityAddressKey(0, CONTACT_INFO, PHONE_NUMBER)]: officerPhone,
-    [getEntityAddressKey(0, CONTACT_INFO, EMAIL)]: officerEmail,
-  };
-
-  let attorneyPhone;
-  let attorneyEmail;
-  attorneyContactInfo.forEach((contact :Map) => {
-    if (contact.has(PHONE_NUMBER)) {
-      attorneyPhone = contact.getIn([PHONE_NUMBER, 0]);
-    }
-    if (contact.has(EMAIL)) {
-      attorneyEmail = contact.getIn([EMAIL, 0]);
-    }
-  });
-
-  originalFormData[getPageSectionKey(1, 5)] = {
-    [getEntityAddressKey(0, CONTACT_INFO, PHONE_NUMBER)]: attorneyPhone,
-    [getEntityAddressKey(0, CONTACT_INFO, EMAIL)]: attorneyEmail,
-  };
-
-  return originalFormData;
-};
-
 const preprocessContactInfoFormData = (formData :Object) :Object => {
   let updatedFormData = formData;
   const pageSection1 :string = getPageSectionKey(1, 1);
@@ -211,6 +126,5 @@ const getNewContactValueFromEditedData = (formData :Object, officerContactInfo :
 export {
   formatGridData,
   getNewContactValueFromEditedData,
-  getOriginalFormData,
   preprocessContactInfoFormData,
 };
